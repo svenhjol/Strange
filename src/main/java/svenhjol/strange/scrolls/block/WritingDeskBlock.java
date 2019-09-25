a46021bd6d64ab60da20670963dd14d049434832
Sven <sven.hjol@protonmail.com>
2019-09-25 13:49:15 +0000

    Added writing desk WIP

---
+++ src/main/java/svenhjol/strange/scrolls/block/WritingDeskBlock.java
@@ -0,0 +1,57 @@
+package svenhjol.strange.scrolls.block;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.HorizontalBlock;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
+import net.minecraft.item.BlockItemUseContext;
+import net.minecraft.state.DirectionProperty;
+import net.minecraft.state.StateContainer;
+import net.minecraft.util.Direction;
+import net.minecraft.util.Mirror;
+import net.minecraft.util.Rotation;
+import svenhjol.meson.MesonModule;
+import svenhjol.meson.block.MesonBlock;
+
+import javax.annotation.Nullable;
+
+public class WritingDeskBlock extends MesonBlock
+{
+    public static final DirectionProperty FACING = HorizontalBlock.HORIZONTAL_FACING;
+
+    public WritingDeskBlock(MesonModule module)
+    {
+        super(module, "writing_desk", Block.Properties
+            .create(Material.WOOD)
+            .hardnessAndResistance(2.5F)
+            .sound(SoundType.WOOD));
+
+        this.setDefaultState(this.getStateContainer().getBaseState().with(FACING, Direction.NORTH));
+    }
+
+    @Nullable
+    @Override
+    public BlockState getStateForPlacement(BlockItemUseContext context)
+    {
+        return this.getDefaultState().with(FACING, context.getPlacementHorizontalFacing().getOpposite());
+    }
+
+    @Override
+    public BlockState rotate(BlockState state, Rotation rotation)
+    {
+        return state.with(FACING, rotation.rotate(state.get(FACING)));
+    }
+
+    @Override
+    public BlockState mirror(BlockState state, Mirror mirror)
+    {
+        return state.rotate(mirror.toRotation(state.get(FACING)));
+    }
+
+    @Override
+    protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder)
+    {
+        builder.add(FACING);
+    }
+}
