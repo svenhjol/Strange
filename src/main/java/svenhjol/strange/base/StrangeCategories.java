c37c28e196b676f60647e38d1506c67661c0ca46
Sven <sven.hjol@protonmail.com>
2019-10-01 01:53:48 +0100

    Reorganise

--- src/main/java/svenhjol/strange/base/StrangeCategories.java
+++ src/main/java/svenhjol/strange/base/StrangeCategories.java
@@ -2,7 +2,8 @@ package svenhjol.strange.base;
 
 public class StrangeCategories
 {
+    public static final String RUNESTONES = "Runestones";
     public static final String SCROLLS = "Scrolls";
+    public static final String STONE_CIRCLES = "StoneCircles";
     public static final String TOTEMS = "Totems";
-    public static final String TRAVEL_RUNES = "TravelRunes";
 }
