841cd5cb8562df222c9b781f79ea583dac6fbc45
Sven <sven.hjol@protonmail.com>
2019-10-03 18:41:24 +0100

    Flying totem is now actually good

---
+++ src/main/java/svenhjol/strange/totems/client/TotemOfFlyingSound.java
@@ -0,0 +1,54 @@
+package svenhjol.strange.totems.client;
+
+import net.minecraft.client.audio.TickableSound;
+import net.minecraft.client.entity.player.ClientPlayerEntity;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.SoundEvents;
+import net.minecraft.util.math.MathHelper;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+
+@OnlyIn(Dist.CLIENT)
+public class TotemOfFlyingSound extends TickableSound
+{
+    private final ClientPlayerEntity player;
+    private int time;
+
+    public TotemOfFlyingSound(ClientPlayerEntity player) {
+        super(SoundEvents.ITEM_ELYTRA_FLYING, SoundCategory.PLAYERS);
+        this.player = player;
+        this.repeat = true;
+        this.repeatDelay = 0;
+        this.volume = 0.1F;
+    }
+
+    public void tick() {
+        ++this.time;
+        if (!this.player.removed && (this.time <= 20 || this.player.abilities.isFlying)) {
+            this.x = (float)this.player.posX;
+            this.y = (float)this.player.posY;
+            this.z = (float)this.player.posZ;
+            float f = (float)this.player.getMotion().lengthSquared();
+            if ((double)f >= 1.0E-7D) {
+                this.volume = MathHelper.clamp(f / 4.0F, 0.0F, 1.0F);
+            } else {
+                this.volume = 0.0F;
+            }
+
+            if (this.time < 20) {
+                this.volume = 0.0F;
+            } else if (this.time < 40) {
+                this.volume = (float)((double)this.volume * ((double)(this.time - 20) / 20.0D));
+            }
+
+            if (this.volume > 0.8F) {
+                this.pitch = 1.0F + (this.volume - 0.8F);
+            } else {
+                this.pitch = 1.0F;
+            }
+
+        } else {
+            this.donePlaying = true;
+        }
+    }
+}
--- src/main/java/svenhjol/strange/totems/module/TotemOfFlying.java
+++ src/main/java/svenhjol/strange/totems/module/TotemOfFlying.java
@@ -1,16 +1,19 @@
 package svenhjol.strange.totems.module;
 
-import net.minecraft.block.Blocks;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.entity.player.ClientPlayerEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.particles.ParticleTypes;
 import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.event.TickEvent.PlayerTickEvent;
+import net.minecraftforge.event.entity.living.LivingEvent.LivingJumpEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import svenhjol.meson.MesonModule;
 import svenhjol.meson.handler.PacketHandler;
@@ -20,7 +23,8 @@ import svenhjol.meson.iface.Module;
 import svenhjol.strange.Strange;
 import svenhjol.strange.base.StrangeCategories;
 import svenhjol.strange.base.TotemHelper;
-import svenhjol.strange.base.message.UpdateTotemMessage;
+import svenhjol.strange.base.message.UpdateFlying;
+import svenhjol.strange.totems.client.TotemOfFlyingSound;
 import svenhjol.strange.totems.item.TotemOfFlyingItem;
 
 @Module(mod = Strange.MOD_ID, category = StrangeCategories.TOTEMS, hasSubscriptions = true)
@@ -29,10 +33,10 @@ public class TotemOfFlying extends MesonModule
     public static TotemOfFlyingItem item;
 
     @Config(name = "Durability", description = "Durability of the Totem.")
-    public static int durability = 128;
+    public static int durability = 64;
 
-    @Config(name = "Chance of damage", description = "Chance (out of 1.0) of damaging the totem every while using it (every 5 ticks).")
-    public static double damageChance = 0.1D;
+    @Config(name = "XP cost", description = "Amount of XP consumed every second (20 ticks) while flying.")
+    public static int xpCost = 1;
 
     @Override
     public void init()
@@ -41,46 +45,63 @@ public class TotemOfFlying extends MesonModule
     }
 
     @SubscribeEvent
+    public void onJump(LivingJumpEvent event)
+    {
+        if (event.getEntityLiving() instanceof PlayerEntity
+            && (event.getEntityLiving().getHeldItemMainhand().getItem() == item
+            || event.getEntityLiving().getHeldItemOffhand().getItem() == item)
+        ) {
+            PlayerEntity player = (PlayerEntity)event.getEntityLiving();
+            ItemStack held = null;
+
+            if (player.experienceTotal <= 0) return;
+
+            for (Hand hand : Hand.values()) {
+                if (player.getHeldItem(hand).getItem() == item) {
+                    held = player.getHeldItem(hand);
+                }
+            }
+            if (held == null) return;
+
+            TotemHelper.damageOrDestroy(player, held, 1);
+            if (player.world.isRemote) {
+                effectStartFlying(player);
+            }
+            enableFlight(player);
+        }
+    }
+
+    @SubscribeEvent
     public void onPlayerTick(PlayerTickEvent event)
     {
         if (event.phase == TickEvent.Phase.START
             && event.player != null
-            && event.player.world.getGameTime() % 5 == 0
+            && event.player.world.getGameTime() % 4 == 0
         ) {
             PlayerEntity player = event.player;
-            BlockPos playerPos = player.getPosition();
+            World world = player.world;
 
-            if (player.getHeldItemMainhand().getItem() == item
-                || player.getHeldItemOffhand().getItem() == item) {
-
-                if (!player.abilities.isFlying && player.world.getBlockState(playerPos.down()).getBlock() == Blocks.BEACON) {
-                    enableFlight(player);
-                }
-
+            if (player.getHeldItemMainhand().getItem() == item || player.getHeldItemOffhand().getItem() == item) {
                 if (player.abilities.isFlying) {
+                    if (!world.isRemote && world.getGameTime() % 20 == 0) {
+                        int xp = player.experienceTotal;
+                        if (xp <= 0) {
+                            disableFlight(player);
+                            PacketHandler.sendTo(new UpdateFlying(UpdateFlying.DISABLE), (ServerPlayerEntity) player);
+                            return;
+                        }
 
-                    if (!player.world.isRemote && player.world.rand.nextDouble() < damageChance) {
                         for (Hand hand : Hand.values()) {
                             ItemStack held = player.getHeldItem(hand);
                             if (held.getItem() != item) continue;
-                            PacketHandler.sendTo(new UpdateTotemMessage(UpdateTotemMessage.DAMAGE, playerPos), (ServerPlayerEntity) player);
-                            int damage = TotemHelper.damage(player, held, 1);
-
-                            if (damage > held.getMaxDamage()) {
-                                PacketHandler.sendTo(new UpdateTotemMessage(UpdateTotemMessage.DESTROY, playerPos), (ServerPlayerEntity) player);
-                                TotemHelper.destroy(player, held);
-                                disableFlight(player);
-                                return;
-                            }
+                            player.giveExperiencePoints(-xpCost);
                         }
                     }
-
-                    if (player.world.isRemote) {
-                        effectFlying(playerPos);
+                    if (world.isRemote) {
+                        effectFlying(player);
                     }
-
-                    return;
                 }
+                return;
             }
 
             disableFlight(player);
@@ -102,15 +123,46 @@ public class TotemOfFlying extends MesonModule
         if (player.isCreative() || player.isSpectator()) {
             player.abilities.allowFlying = true;
         } else {
-            player.abilities.isFlying = true;
             player.abilities.allowFlying = true;
+            player.abilities.isFlying = true;
         }
     }
 
     @OnlyIn(Dist.CLIENT)
-    public static void effectFlying(BlockPos pos)
+    public static void disableFlight()
+    {
+        PlayerEntity player = ClientHelper.getClientPlayer();
+        if (player.isCreative() || player.isSpectator()) {
+            player.abilities.allowFlying = true;
+        } else{
+            player.abilities.isFlying = false;
+            player.abilities.allowFlying = false;
+        }
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    public static void enableFlight()
+    {
+        PlayerEntity player = ClientHelper.getClientPlayer();
+        if (player.isCreative() || player.isSpectator()) {
+            player.abilities.allowFlying = true;
+        } else {
+            player.abilities.allowFlying = true;
+            player.abilities.isFlying = true;
+        }
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    public static void effectStartFlying(PlayerEntity player)
+    {
+        Minecraft.getInstance().getSoundHandler().play(new TotemOfFlyingSound((ClientPlayerEntity)player));
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    public static void effectFlying(PlayerEntity player)
     {
         double spread = 0.6D;
+        BlockPos pos = player.getPosition();
         for (int i = 0; i < 8; i++) {
             double px = pos.getX() + 0.5D + (Math.random() - 0.5D) * spread;
             double py = pos.getY() - 0.4D + (Math.random() - 0.5D) * spread;
