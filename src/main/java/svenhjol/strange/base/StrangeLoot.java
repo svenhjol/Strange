7f55f8134e4825e46c634db955d4a9e25663cc1c
Sven <sven.hjol@protonmail.com>
2019-11-02 18:23:24 +0000

    Loot table loading for stone circle treasure

--- src/main/java/svenhjol/strange/base/StrangeLoot.java
+++ src/main/java/svenhjol/strange/base/StrangeLoot.java
@@ -8,10 +8,12 @@ public class StrangeLoot
     public static final ResourceLocation CHESTS_VAULT_BOOKSHELVES;
     public static final ResourceLocation CHESTS_VAULT_STORAGE;
     public static final ResourceLocation CHESTS_VAULT_TREASURE;
+    public static final ResourceLocation STONE_CIRCLE_TREASURE;
 
     static {
         CHESTS_VAULT_BOOKSHELVES = new ResourceLocation(Strange.MOD_ID, "chests/vaults_bookshelves");
         CHESTS_VAULT_STORAGE = new ResourceLocation(Strange.MOD_ID, "chests/vaults_storage");
         CHESTS_VAULT_TREASURE = new ResourceLocation(Strange.MOD_ID, "chests/vaults_treasure");
+        STONE_CIRCLE_TREASURE = new ResourceLocation(Strange.MOD_ID, "chests/stone_circle_treasure");
     }
 }
