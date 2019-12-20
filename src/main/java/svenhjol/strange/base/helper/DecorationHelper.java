3063d8f433592398498738ec4898a274fd5879f7
Sven <sven.hjol@protonmail.com>
2019-12-20 14:22:37 +0000

    Refactor and improve decorations

--- src/main/java/svenhjol/strange/base/helper/DecorationHelper.java
+++ src/main/java/svenhjol/strange/base/helper/DecorationHelper.java
@@ -24,9 +24,13 @@ import svenhjol.charm.decoration.module.BookshelfChests;
 import svenhjol.charm.decoration.module.Crates;
 import svenhjol.charm.decoration.module.GoldLanterns;
 import svenhjol.charm.decoration.tileentity.BookshelfChestTileEntity;
+import svenhjol.meson.Meson;
 import svenhjol.meson.enums.WoodType;
+import svenhjol.meson.helper.ForgeHelper;
 import svenhjol.meson.helper.LootHelper;
 import svenhjol.strange.Strange;
+import svenhjol.strange.base.compat.QuarkCaveRoots;
+import svenhjol.strange.base.compat.QuarkVariantChests;
 import svenhjol.strange.ruins.tile.EntitySpawnerTileEntity;
 import svenhjol.strange.runestones.module.Runestones;
 import svenhjol.strange.scrolls.block.WritingDeskBlock;
@@ -39,13 +43,18 @@ import java.util.regex.Pattern;
 
 public class DecorationHelper
 {
+    public static QuarkVariantChests quarkVariantChests;
+    public static QuarkCaveRoots quarkCaveRoots;
+
     public static final String ANVIL = "anvil";
     public static final String ARMOR = "armor";
+    public static final String BLOCK = "block";
     public static final String BOOKSHELF = "bookshelf";
     public static final String CARPET = "carpet";
     public static final String CAULDRON = "cauldron";
     public static final String CHEST = "chest";
     public static final String DECORATION = "decoration";
+    public static final String ENTITY = "entity";
     public static final String FLOWER = "flower";
     public static final String LANTERN = "lantern";
     public static final String LAVA = "lava";
@@ -118,18 +127,103 @@ public class DecorationHelper
     );
 
     public static List<Block> carpetTypes = Arrays.asList(
-        Blocks.WHITE_CARPET,
         Blocks.ORANGE_CARPET,
         Blocks.RED_CARPET,
         Blocks.YELLOW_CARPET,
         Blocks.LIME_CARPET,
+        Blocks.GREEN_CARPET,
         Blocks.CYAN_CARPET,
         Blocks.BLUE_CARPET,
+        Blocks.LIGHT_BLUE_CARPET,
         Blocks.PURPLE_CARPET,
-        Blocks.BLACK_CARPET
+        Blocks.MAGENTA_CARPET,
+        Blocks.PINK_CARPET,
+        Blocks.BLACK_CARPET,
+        Blocks.GRAY_CARPET,
+        Blocks.LIGHT_GRAY_CARPET,
+        Blocks.BROWN_CARPET
     );
 
+    public static List<ResourceLocation> storageLootTables = Arrays.asList(
+        LootTables.CHESTS_SHIPWRECK_SUPPLY,
+        LootTables.CHESTS_VILLAGE_VILLAGE_PLAINS_HOUSE,
+        LootTables.CHESTS_VILLAGE_VILLAGE_DESERT_HOUSE,
+        LootTables.CHESTS_VILLAGE_VILLAGE_SAVANNA_HOUSE,
+        LootTables.CHESTS_VILLAGE_VILLAGE_SNOWY_HOUSE,
+        LootTables.CHESTS_VILLAGE_VILLAGE_TAIGA_HOUSE,
+        LootTables.CHESTS_VILLAGE_VILLAGE_BUTCHER
+    );
+
+    public static List<ResourceLocation> chestLootTables = Arrays.asList(
+        LootTables.CHESTS_SIMPLE_DUNGEON,
+        LootTables.CHESTS_SHIPWRECK_TREASURE
+    );
+
+    public static List<Block> decorationTypes = new ArrayList<>(Arrays.asList(
+        Blocks.BLAST_FURNACE,
+        Blocks.FURNACE,
+        Blocks.SMOKER,
+        Blocks.CARVED_PUMPKIN,
+        Blocks.DISPENSER,
+        Blocks.OBSERVER,
+        Blocks.LECTERN,
+        Blocks.TRAPPED_CHEST,
+        Blocks.CAULDRON,
+        Blocks.COMPOSTER,
+        Blocks.BREWING_STAND,
+        Blocks.STONECUTTER,
+        Blocks.PUMPKIN,
+        Blocks.MELON,
+        Blocks.LANTERN,
+        Blocks.COBWEB,
+        Blocks.HAY_BLOCK,
+        Blocks.JUKEBOX,
+        Blocks.NOTE_BLOCK,
+        Blocks.FLETCHING_TABLE,
+        Blocks.SMITHING_TABLE,
+        Blocks.CRAFTING_TABLE,
+        Blocks.CARTOGRAPHY_TABLE,
+        Blocks.ANVIL,
+        Blocks.CHIPPED_ANVIL,
+        Blocks.DAMAGED_ANVIL,
+        Blocks.BELL,
+        Blocks.CHEST,
+        Blocks.BARREL,
+        Blocks.POTTED_CACTUS,
+        Blocks.POLISHED_GRANITE,
+        Blocks.POLISHED_DIORITE,
+        Blocks.POLISHED_ANDESITE,
+        Blocks.IRON_BLOCK,
+        Blocks.GOLD_BLOCK,
+        Blocks.EMERALD_BLOCK,
+        Blocks.EMERALD_ORE,
+        Blocks.LAPIS_BLOCK,
+        Blocks.DIAMOND_ORE,
+        Blocks.GOLD_ORE,
+        Blocks.WHITE_WOOL,
+        Blocks.BLACK_WOOL,
+        Blocks.COAL_BLOCK,
+        Blocks.BONE_BLOCK,
+        Blocks.SLIME_BLOCK
+    ));
+
     public static List<WoodType> woodTypes = new ArrayList<>(Arrays.asList(WoodType.values()));
+
+    static {
+        try {
+            if (ForgeHelper.isModLoaded("quark")) {
+                quarkVariantChests = QuarkVariantChests.class.newInstance();
+                quarkCaveRoots = QuarkCaveRoots.class.newInstance();
+            }
+        } catch (Exception e) {
+            Meson.warn("Error loading Quark compat class", e);
+        }
+
+        if (Strange.loader.hasModule(Scrollkeepers.class))
+            decorationTypes.add(Scrollkeepers.block);
+    }
+
+    public static StructureBlockReplacement STRUCTURE_BLOCK_INSTANCE = new StructureBlockReplacement();
 
     public static class StructureBlockReplacement
     {
@@ -141,21 +235,17 @@ public class DecorationHelper
         protected Random fixedRand;
         protected Random rand;
         protected BlockInfo blockInfo;
+        protected float chance;
 
-        public StructureBlockReplacement(Rotation rotation, Random fixedRand)
+        public BlockInfo replace(Rotation rotation, BlockInfo blockInfo, String data, Random rand)
         {
-            this.fixedRand = fixedRand;
+            this.fixedRand = rand;
             this.rotation = rotation;
-        }
-
-        public BlockInfo replace(BlockInfo blockInfo, String data)
-        {
             this.blockInfo = blockInfo;
             this.pos = blockInfo.pos;
             this.state = null;
             this.nbt = null;
-            this.rand = new Random(blockInfo.hashCode());
-
+            this.rand = new Random(pos.toLong());
 
             if (data.contains("|")) {
                 String[] split = data.split("\\|");
@@ -163,14 +253,17 @@ public class DecorationHelper
             }
 
             this.data = data.trim();
+            this.chance = getChance(this.data, 0.0F);
 
             if (this.data.startsWith(ANVIL)) anvil();
             if (this.data.startsWith(ARMOR)) armorStand();
+            if (this.data.startsWith(BLOCK)) block();
             if (this.data.startsWith(BOOKSHELF)) bookshelf();
             if (this.data.startsWith(CARPET)) carpet();
             if (this.data.startsWith(CAULDRON)) cauldron();
             if (this.data.startsWith(CHEST)) chest();
             if (this.data.startsWith(DECORATION)) decoration();
+            if (this.data.startsWith(ENTITY)) entity();
             if (this.data.startsWith(FLOWER)) flower();
             if (this.data.startsWith(LANTERN)) lantern();
             if (this.data.startsWith(LAVA)) lava();
@@ -216,10 +309,22 @@ public class DecorationHelper
             this.state = EntitySpawner.block.getDefaultState();
         }
 
+        protected void block()
+        {
+            if (!withChance(0.8F)) return;
+            String type = getValue("type", this.data, "");
+            if (type.isEmpty()) return;
+
+            Block block = ForgeRegistries.BLOCKS.getValue(new ResourceLocation(type));
+            if (block == null) return;
+
+            this.state = block.getDefaultState();
+        }
+
         protected void bookshelf()
         {
             // TODO variant bookshelves
-            if (Charm.hasModule(BookshelfChests.class) && rand.nextFloat() < getChance(this.data, 0.22F)) {
+            if (Charm.hasModule(BookshelfChests.class) && withChance(0.22F)) {
                 state = ((Block) BookshelfChests.blocks.get(WoodType.OAK)).getDefaultState()
                     .with(BookshelfChestBlock.SLOTS, 9);
 
@@ -252,17 +357,20 @@ public class DecorationHelper
 
         protected void chest()
         {
-            if (rand.nextFloat() > getChance(this.data, 0.66F)) return;
+            Block chest = null;
 
-            state = Blocks.CHEST.getDefaultState();
+            if (!withChance(0.66F)) return;
+
+            if (quarkVariantChests != null && quarkVariantChests.hasModule() && rand.nextFloat() < 0.75F)
+                chest = quarkVariantChests.getRandomChest(rand);
+
+            if (chest == null)
+                chest = Blocks.CHEST;
+
+            state = chest.getDefaultState();
             state = setFacing(state, ChestBlock.FACING, getValue("facing", this.data, "north"));
 
-            List<ResourceLocation> tables = Arrays.asList(
-                LootTables.CHESTS_SIMPLE_DUNGEON,
-                LootTables.CHESTS_SHIPWRECK_TREASURE
-            );
-            ResourceLocation defaultLoot = tables.get(rand.nextInt(tables.size()));
-
+            ResourceLocation defaultLoot = chestLootTables.get(rand.nextInt(chestLootTables.size()));
             ChestTileEntity tile = TileEntityType.CHEST.create();
             if (tile != null) {
                 tile.setLootTable(getLootTable(this.data, defaultLoot), rand.nextLong());
@@ -273,56 +381,55 @@ public class DecorationHelper
 
         protected void decoration()
         {
-            if (rand.nextFloat() > getChance(this.data, 0.85F)) return;
+            if (!withChance(0.85F)) return;
 
             Direction facing = getFacing(getValue("facing", this.data, "north"));
-            List<BlockState> types = new ArrayList<>(Arrays.asList(
-                Blocks.BLAST_FURNACE.getDefaultState().with(BlastFurnaceBlock.FACING, facing),
-                Blocks.FURNACE.getDefaultState().with(FurnaceBlock.FACING, facing),
-                Blocks.SMOKER.getDefaultState().with(SmokerBlock.FACING, facing),
-                Blocks.CARVED_PUMPKIN.getDefaultState().with(CarvedPumpkinBlock.FACING, facing),
-                Blocks.DISPENSER.getDefaultState().with(DispenserBlock.FACING, facing),
-                Blocks.OBSERVER.getDefaultState().with(ObserverBlock.FACING, facing),
-                Blocks.LECTERN.getDefaultState().with(LecternBlock.FACING, facing),
-                Blocks.TRAPPED_CHEST.getDefaultState().with(TrappedChestBlock.FACING, facing),
-                Blocks.CAMPFIRE.getDefaultState().with(CampfireBlock.LIT, false),
-                Blocks.CAULDRON.getDefaultState().with(CauldronBlock.LEVEL, rand.nextInt(3)),
-                Blocks.COMPOSTER.getDefaultState().with(ComposterBlock.LEVEL, rand.nextInt(7)),
-                Blocks.BREWING_STAND.getDefaultState(),
-                Blocks.STONECUTTER.getDefaultState(),
-                Blocks.PUMPKIN.getDefaultState(),
-                Blocks.MELON.getDefaultState(),
-                Blocks.LANTERN.getDefaultState(),
-                Blocks.COBWEB.getDefaultState(),
-                Blocks.HAY_BLOCK.getDefaultState(),
-                Blocks.JUKEBOX.getDefaultState(),
-                Blocks.NOTE_BLOCK.getDefaultState(),
-                Blocks.FLETCHING_TABLE.getDefaultState(),
-                Blocks.SMITHING_TABLE.getDefaultState(),
-                Blocks.CRAFTING_TABLE.getDefaultState(),
-                Blocks.CARTOGRAPHY_TABLE.getDefaultState(),
-                Blocks.ANVIL.getDefaultState(),
-                Blocks.CHIPPED_ANVIL.getDefaultState(),
-                Blocks.DAMAGED_ANVIL.getDefaultState(),
-                Blocks.BELL.getDefaultState()
-            ));
+            Block block = decorationTypes.get(rand.nextInt(decorationTypes.size()));
+            state = block.getDefaultState();
 
-            if (Strange.loader.hasModule(Scrollkeepers.class)) {
-                types.add(Scrollkeepers.block.getDefaultState().with(WritingDeskBlock.FACING, facing));
-            }
+            if (block == Blocks.BLAST_FURNACE) state = state.with(BlastFurnaceBlock.FACING, facing);
+            if (block == Blocks.FURNACE) state = state.with(FurnaceBlock.FACING, facing);
+            if (block == Blocks.SMOKER) state = state.with(SmokerBlock.FACING, facing);
+            if (block == Blocks.CARVED_PUMPKIN) state = state.with(CarvedPumpkinBlock.FACING, facing);
+            if (block == Blocks.DISPENSER) state = state.with(DispenserBlock.FACING, facing);
+            if (block == Blocks.OBSERVER) state = state.with(ObserverBlock.FACING, facing);
+            if (block == Blocks.LECTERN) state = state.with(LecternBlock.FACING, facing);
+            if (block == Blocks.TRAPPED_CHEST) state = state.with(TrappedChestBlock.FACING, facing);
+            if (block == Blocks.CAMPFIRE) state = state.with(CampfireBlock.LIT, false);
+            if (block == Blocks.CAULDRON) state = state.with(CauldronBlock.LEVEL, rand.nextInt(3));
+            if (block == Blocks.COMPOSTER) state = state.with(ComposterBlock.LEVEL, rand.nextInt(7));
+            if (block == Scrollkeepers.block) state = state.with(WritingDeskBlock.FACING, facing);
+        }
 
-            state = types.get(rand.nextInt(types.size()));
+        protected void entity()
+        {
+            EntitySpawnerTileEntity tile = EntitySpawner.tile.create();
+            if (tile == null) return;
+            nbt = new CompoundNBT();
+
+            String type = getValue("type", this.data, "");
+            if (type.isEmpty()) return;
+
+            EntityType<?> ent = ForgeRegistries.ENTITIES.getValue(new ResourceLocation(type));
+            if (ent == null) return;
+
+            tile.entity = ent.getRegistryName();
+            tile.meta = this.data;
+            tile.rotation = this.rotation;
+            tile.write(this.nbt);
+
+            this.state = EntitySpawner.block.getDefaultState();
         }
 
         protected void flower()
         {
-            if (rand.nextFloat() > getChance(this.data, 0.8F)) return;
+            if (!withChance(0.8F)) return;
             state = flowerTypes.get(rand.nextInt(flowerTypes.size())).getDefaultState();
         }
 
         protected void lantern()
         {
-            if (rand.nextFloat() > getChance(this.data, 0.9F)) return;
+            if (!withChance(0.9F)) return;
 
             state = Blocks.LANTERN.getDefaultState();
 
@@ -346,7 +453,7 @@ public class DecorationHelper
 
         protected void lectern()
         {
-            if (rand.nextFloat() > getChance(this.data, 0.8F)) return;
+            if (!withChance(0.8F)) return;
 
             state = Blocks.LECTERN.getDefaultState();
             state = setFacing(state, LecternBlock.FACING, getValue("facing", this.data, "north"));
@@ -354,7 +461,7 @@ public class DecorationHelper
 
         protected void mob()
         {
-            if (rand.nextFloat() > getChance(this.data, 0.75F)) return;
+            if (!withChance(0.75F)) return;
 
             EntitySpawnerTileEntity tile = EntitySpawner.tile.create();
             if (tile == null) return;
@@ -375,7 +482,7 @@ public class DecorationHelper
 
         protected void ore()
         {
-            if (rand.nextFloat() > getChance(this.data, 0.8F)) return;
+            if (!withChance(0.8F)) return;
 
             String type = getValue("type", this.data, "");
             if (!type.isEmpty()) {
@@ -391,13 +498,13 @@ public class DecorationHelper
 
         protected void potted()
         {
-            if (rand.nextFloat() > getChance(this.data, 0.8F)) return;
+            if (!withChance(0.8F)) return;
             state = pottedTypes.get(rand.nextInt(pottedTypes.size())).getDefaultState();
         }
 
         protected void rune()
         {
-            if (!Strange.loader.hasModule(Runestones.class) || rand.nextFloat() > getChance(this.data, 0.75F)) return;
+            if (!Strange.loader.hasModule(Runestones.class) || !withChance(0.75F)) return;
             String type = getValue("type", this.data, "overworld");
 
             switch (type) {
@@ -415,13 +522,13 @@ public class DecorationHelper
 
         protected void sapling()
         {
-            if (rand.nextFloat() > getChance(this.data, 0.8F)) return;
+            if (!withChance(0.8F)) return;
             state = saplingTypes.get(rand.nextInt(saplingTypes.size())).getDefaultState();
         }
 
         protected void spawner()
         {
-            if (rand.nextFloat() > getChance(this.data, 0.8F)) return;
+            if (!withChance(0.8F)) return;
             state = Blocks.SPAWNER.getDefaultState();
 
             MobSpawnerTileEntity tile = TileEntityType.MOB_SPAWNER.create();
@@ -435,7 +542,7 @@ public class DecorationHelper
 
         protected void storage()
         {
-            if (rand.nextFloat() > getChance(this.data, 0.7F)) return;
+            if (!withChance(0.7F)) return;
             LockableLootTileEntity tile;
             WoodType woodType = woodTypes.get(fixedRand.nextInt(woodTypes.size()));
 
@@ -461,16 +568,7 @@ public class DecorationHelper
                 tile = TileEntityType.BARREL.create();
             }
 
-            List<ResourceLocation> tables = Arrays.asList(
-                LootTables.CHESTS_SHIPWRECK_SUPPLY,
-                LootTables.CHESTS_VILLAGE_VILLAGE_PLAINS_HOUSE,
-                LootTables.CHESTS_VILLAGE_VILLAGE_DESERT_HOUSE,
-                LootTables.CHESTS_VILLAGE_VILLAGE_SAVANNA_HOUSE,
-                LootTables.CHESTS_VILLAGE_VILLAGE_SNOWY_HOUSE,
-                LootTables.CHESTS_VILLAGE_VILLAGE_TAIGA_HOUSE,
-                LootTables.CHESTS_VILLAGE_VILLAGE_BUTCHER
-            );
-            ResourceLocation defaultLoot = tables.get(rand.nextInt(tables.size()));
+            ResourceLocation defaultLoot = storageLootTables.get(rand.nextInt(storageLootTables.size()));
 
             if (tile != null) {
                 tile.setLootTable(getLootTable(this.data, defaultLoot), rand.nextLong());
@@ -479,25 +577,16 @@ public class DecorationHelper
             }
         }
 
+        public boolean withChance(float chance)
+        {
+            float f = this.rand.nextFloat();
+            return this.chance > 0 ? f < this.chance : f < chance;
+        }
+
         public float getChance(String data, float def)
         {
             int i = getValue("chance", data, 0);
             return i == 0 ? def : ((float)i)/100.0F;
-        }
-
-        public ResourceLocation getLootTable(String data, ResourceLocation def)
-        {
-            ResourceLocation lootTable = def;
-            String loot = getValue("loot", data, "");
-            if (!loot.isEmpty()) {
-                List<ResourceLocation> tables = LootHelper.getLootTables();
-                for (ResourceLocation res : tables) {
-                    String[] s = res.getPath().split("/");
-                    if (loot.contains(s[s.length - 1]))
-                        lootTable = res;
-                }
-            }
-            return lootTable;
         }
     }
 
@@ -545,5 +634,20 @@ public class DecorationHelper
             if (m.find()) return m.group(1);
         }
         return def;
+    }
+
+    public static ResourceLocation getLootTable(String data, ResourceLocation def)
+    {
+        ResourceLocation lootTable = def;
+        String loot = getValue("loot", data, "");
+        if (!loot.isEmpty()) {
+            List<ResourceLocation> tables = LootHelper.getLootTables();
+            for (ResourceLocation res : tables) {
+                String[] s = res.getPath().split("/");
+                if (loot.contains(s[s.length - 1]))
+                    lootTable = res;
+            }
+        }
+        return lootTable;
     }
 }
