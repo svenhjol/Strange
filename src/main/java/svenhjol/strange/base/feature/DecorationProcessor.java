3eed7aff511d0ecf3e998e8825900d6acc6088e4
Sven <sven.hjol@protonmail.com>
2019-12-20 14:20:11 +0000

    Merged everything into DecorationProcessor

--- src/main/java/svenhjol/strange/base/feature/DecorationProcessor.java
+++ src/main/java/svenhjol/strange/base/feature/DecorationProcessor.java
@@ -4,6 +4,7 @@ import com.mojang.datafixers.Dynamic;
 import com.mojang.datafixers.types.DynamicOps;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.material.Material;
+import net.minecraft.state.properties.StructureMode;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.world.IWorldReader;
@@ -13,19 +14,22 @@ import net.minecraft.world.gen.feature.template.StructureProcessor;
 import net.minecraft.world.gen.feature.template.Template;
 import net.minecraft.world.gen.feature.template.Template.BlockInfo;
 import svenhjol.strange.Strange;
+import svenhjol.strange.base.helper.DecorationHelper;
 
 import javax.annotation.Nullable;
+import java.util.Random;
 
-public class AirBlockProcessor extends StructureProcessor
+public class DecorationProcessor extends StructureProcessor
 {
-    private static final IStructureProcessorType TYPE = Registry.register(Registry.STRUCTURE_PROCESSOR, Strange.MOD_ID + ":air_block", AirBlockProcessor::new);
+    private static final IStructureProcessorType TYPE = Registry.register(Registry.STRUCTURE_PROCESSOR, Strange.MOD_ID + ":air_block", DecorationProcessor::new);
+    private Random rand;
 
-    public AirBlockProcessor()
+    public DecorationProcessor()
     {
-        // no op
+        this.rand = new Random();
     }
 
-    public AirBlockProcessor(Dynamic<?> dynamic)
+    public DecorationProcessor(Dynamic<?> dynamic)
     {
         this();
     }
@@ -34,15 +38,28 @@ public class AirBlockProcessor extends StructureProcessor
     @Override
     public BlockInfo process(IWorldReader world, BlockPos pos, BlockInfo unused, BlockInfo blockInfo, PlacementSettings placement, @Nullable Template template)
     {
+        // remove air
         if (blockInfo.state.getMaterial() == Material.AIR) {
-//            return new BlockInfo(blockInfo.pos, Blocks.AIR.getDefaultState(), blockInfo.nbt);
-//            if (blockInfo.pos == null) return blockInfo;
-
             if (world.getBlockState(blockInfo.pos.up()).getMaterial().isLiquid())
                 return new BlockInfo(blockInfo.pos, Blocks.WATER.getDefaultState(), null);
 
             if (pos.getY() < world.getSeaLevel())
                 return new BlockInfo(blockInfo.pos, Blocks.CAVE_AIR.getDefaultState(), null);
+        }
+
+        // remove cave roots - TODO probably isn't working
+        if (DecorationHelper.quarkCaveRoots != null
+            && blockInfo.state.getBlock() == DecorationHelper.quarkCaveRoots.getRootBlock()
+        ) {
+            return new BlockInfo(blockInfo.pos, Blocks.CAVE_AIR.getDefaultState(), null);
+        }
+
+        // replace structure blocks with decoration
+        if (blockInfo.state.getBlock() == Blocks.STRUCTURE_BLOCK) {
+            StructureMode mode = StructureMode.valueOf(blockInfo.nbt.getString("mode"));
+            if (mode == StructureMode.DATA) {
+                return DecorationHelper.STRUCTURE_BLOCK_INSTANCE.replace(placement.getRotation(), blockInfo, blockInfo.nbt.getString("metadata"), this.rand);
+            }
         }
 
         return blockInfo;
