777207d716bcecdbbf26cadb7ae9f2ffb20c1060
Steve A <tolta@tutanota.com>
2019-12-31 16:13:10 +0000

    Moved helper methods to Charm

--- src/main/java/svenhjol/strange/spells/spells/BlinkSpell.java
+++ src/main/java/svenhjol/strange/spells/spells/BlinkSpell.java
@@ -6,7 +6,7 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.world.World;
-import svenhjol.strange.base.helper.StructureHelper;
+import svenhjol.meson.helper.WorldHelper;
 
 import java.util.function.Consumer;
 
@@ -34,9 +34,9 @@ public class BlinkSpell extends Spell
                 BlockPos pos1 = pos.up(1);
                 BlockPos pos2 = pos.up(2);
 
-                if (StructureHelper.isSolidishBlock(world, pos)
-                    && StructureHelper.isAirBlock(world, pos1)
-                    && StructureHelper.isAirBlock(world, pos2)
+                if (WorldHelper.isSolidishBlock(world, pos)
+                    && WorldHelper.isAirBlock(world, pos1)
+                    && WorldHelper.isAirBlock(world, pos2)
                 ) {
                     player.setPositionAndUpdate(pos.getX() + 0.5D, pos.getY() + 1D, pos.getZ() + 0.5D);
                     player.setMotion(0, 0, 0);
