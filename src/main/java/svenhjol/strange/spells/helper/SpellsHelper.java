21f1fc4b5a28afd689c5454a429b23a5b00c75f7
Steve A <tolta@tutanota.com>
2020-01-03 19:53:37 +0000

    More spell helpers

--- src/main/java/svenhjol/strange/spells/helper/SpellsHelper.java
+++ src/main/java/svenhjol/strange/spells/helper/SpellsHelper.java
@@ -2,11 +2,14 @@ package svenhjol.strange.spells.helper;
 
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.Style;
+import net.minecraft.util.text.TextFormatting;
 import net.minecraft.util.text.TranslationTextComponent;
+import svenhjol.meson.helper.StringHelper;
 import svenhjol.strange.spells.module.Spells;
 import svenhjol.strange.spells.spells.Spell;
 
 import javax.annotation.Nullable;
+import java.util.List;
 import java.util.Random;
 
 public class SpellsHelper
@@ -19,18 +22,47 @@ public class SpellsHelper
     public static ITextComponent getSpellInfoText(Spell spell, String translationKey)
     {
         TranslationTextComponent spellText = new TranslationTextComponent(spell.getTranslationKey());
-        spellText.setStyle((new Style()).setColor(spell.getElement().getFormatColor()));
+
+        TextFormatting col = TextFormatting.fromColorIndex(StringHelper.dyeTextMap.get(spell.getColor().getId()));
+        if (col != null) {
+            spellText.setStyle((new Style()).setColor(col));
+        }
+
+        //spellText.setStyle((new Style()).setColor(spell.getColor().getFormatColor()));
         if (translationKey.isEmpty()) {
             return spellText;
         }
         return new TranslationTextComponent(translationKey, spellText);
     }
 
-    public static ITextComponent getSpellElementText(Spell spell)
+    public static void addSpellDescription(Spell spell, List<ITextComponent> tooltip)
     {
-        TranslationTextComponent elementText = new TranslationTextComponent("spell.strange.element." + spell.getElement().getName());
-        elementText.setStyle((new Style()).setColor(spell.getElement().getFormatColor()));
-        return elementText;
+        int cost = spell.getApplyCost();
+
+        // add description
+        ITextComponent descriptionText = new TranslationTextComponent(spell.getDescriptionKey());
+        descriptionText.setStyle((new Style()).setColor(TextFormatting.WHITE));
+        tooltip.add(descriptionText);
+
+        // add text to show what the spell affects
+        ITextComponent affectText = new TranslationTextComponent(spell.getAffectKey());
+        affectText.setStyle((new Style()).setColor(TextFormatting.GRAY));
+        tooltip.add(affectText);
+
+        // add cost if needed
+        if (cost > 0) {
+            String key = cost == 1 ? "spell.strange.level_cost" : "spell.strange.levels_cost";
+            ITextComponent applyCostText = new TranslationTextComponent(key, spell.getApplyCost());
+            applyCostText.setStyle((new Style()).setColor(TextFormatting.GREEN));
+            tooltip.add(applyCostText);
+        }
+
+        // add activation message if needed
+        if (spell.needsActivation()) {
+            ITextComponent activateText = new TranslationTextComponent("spell.strange.needs_activation");
+            activateText.setStyle((new Style()).setColor(TextFormatting.RED));
+            tooltip.add(activateText);
+        }
     }
 
     @Nullable
