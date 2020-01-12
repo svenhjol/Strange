10bd7367778f8ec912719d0b60918d1a3a36f297
Sven <sven.hjol@protonmail.com>
2020-01-12 11:13:02 +0000

    Fix up summon spell now being focus rather than area

--- src/main/java/svenhjol/strange/spells/spells/SummonSpell.java
+++ src/main/java/svenhjol/strange/spells/spells/SummonSpell.java
@@ -1,14 +1,15 @@
 package svenhjol.strange.spells.spells;
 
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.SpawnReason;
 import net.minecraft.entity.monster.MonsterEntity;
 import net.minecraft.entity.passive.IronGolemEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.DyeColor;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
-import svenhjol.meson.helper.PlayerHelper;
 
 import java.util.List;
 import java.util.Objects;
@@ -31,28 +32,29 @@ public class SummonSpell extends Spell
     public void cast(PlayerEntity player, ItemStack stone, Consumer<Boolean> didCast)
     {
         int[] range = {6, 2, 6};
-        this.castFocus(player, pos -> {
+        this.castFocus(player, result -> {
             World world = player.world;
+            BlockPos pos = result.getPos();
 
-            List<MonsterEntity> entities;
-            AxisAlignedBB area = player.getBoundingBox().grow(range[0], range[1], range[2]);
-            Predicate<MonsterEntity> selector = Objects::nonNull;
-            entities = world.getEntitiesWithinAABB(MonsterEntity.class, area, selector);
+            IronGolemEntity fighter = EntityType.IRON_GOLEM.create(world);
+            if (fighter != null) {
+                fighter.getTags().add(SUMMONED);
+                fighter.moveToBlockPosAndAngles(pos.add(0, 1, 0), 0.0F, 0.0F);
+                fighter.onInitialSpawn(world, world.getDifficultyForLocation(pos), SpawnReason.TRIGGERED, null, null);
+                world.addEntity(fighter);
 
-            if (!entities.isEmpty()) {
-                IronGolemEntity fighter = EntityType.IRON_GOLEM.create(world);
-                if (fighter != null) {
-                    fighter.getTags().add(SUMMONED);
+                List<MonsterEntity> entities;
+                AxisAlignedBB area = fighter.getBoundingBox().grow(range[0], range[1], range[2]);
+                Predicate<MonsterEntity> selector = Objects::nonNull;
+                entities = world.getEntitiesWithinAABB(MonsterEntity.class, area, selector);
+
+                if (!entities.isEmpty()) {
                     MonsterEntity target = entities.get(world.rand.nextInt(entities.size()));
-
-                    PlayerHelper.spawnEntityNearPlayer(player, fighter, (mobEntity, blockPos) -> {
-                        IronGolemEntity s = (IronGolemEntity) mobEntity;
-                        s.setAttackTarget(target);
-                    });
-
-                    didCast.accept(true);
-                    return;
+                    fighter.setAttackTarget(target);
                 }
+
+                didCast.accept(true);
+                return;
             }
 
             didCast.accept(false);
