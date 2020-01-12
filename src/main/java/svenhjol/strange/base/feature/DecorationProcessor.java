19e06fb07eef01042162fb4b8c6a253d298590c9
Sven <sven.hjol@protonmail.com>
2020-01-12 10:36:02 +0000

    The continuing saga of the decoration

--- src/main/java/svenhjol/strange/base/feature/DecorationProcessor.java
+++ src/main/java/svenhjol/strange/base/feature/DecorationProcessor.java
@@ -2,9 +2,13 @@ package svenhjol.strange.base.feature;
 
 import com.mojang.datafixers.Dynamic;
 import com.mojang.datafixers.types.DynamicOps;
+import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
+import net.minecraft.block.RailBlock;
 import net.minecraft.block.material.Material;
+import net.minecraft.state.properties.RailShape;
 import net.minecraft.state.properties.StructureMode;
+import net.minecraft.util.Rotation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.world.IWorldReader;
@@ -14,6 +18,7 @@ import net.minecraft.world.gen.feature.template.PlacementSettings;
 import net.minecraft.world.gen.feature.template.StructureProcessor;
 import net.minecraft.world.gen.feature.template.Template;
 import net.minecraft.world.gen.feature.template.Template.BlockInfo;
+import svenhjol.meson.Meson;
 import svenhjol.strange.Strange;
 import svenhjol.strange.base.helper.DecorationHelper;
 
@@ -38,14 +43,31 @@ public class DecorationProcessor extends StructureProcessor
     @Override
     public BlockInfo process(IWorldReader world, BlockPos pos, BlockInfo unused, BlockInfo blockInfo, PlacementSettings placement, @Nullable Template template)
     {
+        Rotation rot = placement.getRotation();
+        Biome biome = null;
+
+        if (world != null) {
+            if (world.getChunk(pos) != null) {
+                if (world.getChunk(pos).getBiomes() != null) {
+                    if (world.getChunk(pos).getBiomes().length > 0) {
+                        biome = world.getChunk(pos).getBiome(pos);
+                    } else {
+                        Meson.debug("[DecorationProcessor] getBiomes().length == 0");
+                    }
+                } else {
+                    Meson.debug("[DecorationProcessor] getBiomes() == null");
+                }
+            } else {
+                Meson.debug("[DecorationProcessor] getChunk(pos) == null");
+            }
+        } else {
+            Meson.debug("[DecorationProcessor] world == null");
+        }
+
         // remove air
         if (blockInfo.state.getMaterial() == Material.AIR) {
-            if (world != null && world.getChunk(pos) != null && world.getChunk(pos).getBiomes() != null && world.getChunk(pos).getBiomes().length > 0) {
-                Biome biome = world.getChunk(pos).getBiome(pos);
-                if (biome.getCategory() == Biome.Category.OCEAN) {
-                    return new BlockInfo(blockInfo.pos, Blocks.WATER.getDefaultState(), null);
-                }
-            }
+            if (biome != null && biome.getCategory() == Biome.Category.OCEAN)
+                return new BlockInfo(blockInfo.pos, Blocks.WATER.getDefaultState(), null);
 
 //            if (world.getBlockState(blockInfo.pos.up()).getMaterial() == Material.WATER)
 //                return new BlockInfo(blockInfo.pos, Blocks.WATER.getDefaultState(), null);
@@ -59,6 +81,21 @@ public class DecorationProcessor extends StructureProcessor
             && blockInfo.state.getBlock() == DecorationHelper.quarkCaveRoots.getRootBlock()
         ) {
             return new BlockInfo(blockInfo.pos, Blocks.CAVE_AIR.getDefaultState(), null);
+        }
+
+        // rotate rails properly
+        if (blockInfo.state.getBlock() == Blocks.RAIL) {
+            if (world.getBlockState(blockInfo.pos.up()).getMaterial().isLiquid())
+                return new BlockInfo(blockInfo.pos, Blocks.CAVE_AIR.getDefaultState(), null);
+
+            BlockState state;
+
+            if (rot == Rotation.CLOCKWISE_90 || rot == Rotation.COUNTERCLOCKWISE_90) {
+                state = blockInfo.state.with(RailBlock.SHAPE, RailShape.NORTH_SOUTH);
+            } else {
+                state = blockInfo.state.with(RailBlock.SHAPE, RailShape.EAST_WEST);
+            }
+            return new BlockInfo(blockInfo.pos, state, null);
         }
 
         // replace structure blocks with decoration
