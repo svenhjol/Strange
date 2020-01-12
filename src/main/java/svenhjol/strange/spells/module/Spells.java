aa36d908f92894742107422f1d3f49e3df8faf43
Sven <sven.hjol@protonmail.com>
2020-01-12 10:41:32 +0000

    Methods for handling meta and uses moved to Spells module

--- src/main/java/svenhjol/strange/spells/module/Spells.java
+++ src/main/java/svenhjol/strange/spells/module/Spells.java
@@ -4,8 +4,12 @@ import com.google.common.base.CaseFormat;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityClassification;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.particles.BasicParticleType;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.SoundCategory;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.server.ServerWorld;
 import net.minecraftforge.api.distmarker.Dist;
@@ -18,6 +22,7 @@ import svenhjol.meson.iface.Config;
 import svenhjol.meson.iface.Module;
 import svenhjol.strange.Strange;
 import svenhjol.strange.base.StrangeCategories;
+import svenhjol.strange.base.StrangeSounds;
 import svenhjol.strange.spells.client.SpellsClient;
 import svenhjol.strange.spells.entity.TargettedSpellEntity;
 import svenhjol.strange.spells.spells.Spell;
@@ -26,10 +31,14 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Consumer;
 
 @Module(mod = Strange.MOD_ID, category = StrangeCategories.SPELLS, hasSubscriptions = true)
 public class Spells extends MesonModule
 {
+    public static final String META = "meta";
+    public static final String USES = "uses";
+
     public static EntityType<? extends Entity> entity;
     public static Map<String, Spell> spells = new HashMap<>();
 
@@ -53,16 +62,6 @@ public class Spells extends MesonModule
         "slowness",
         "summon",
         "transfer"
-    );
-
-    @Config(name = "Common spells", description = "Subset of 'enabled spells' that appear in common dungeon loot and villager trades.")
-    public static List<String> commonSpells = Arrays.asList(
-        "aura",
-        "freeze",
-        "growth",
-        "knockback",
-        "levitate",
-        "slowness"
     );
 
     public static List<String> transferBlacklist = Arrays.asList(
@@ -129,6 +128,85 @@ public class Spells extends MesonModule
     public void setupClient(FMLClientSetupEvent event)
     {
         client = new SpellsClient();
+    }
+
+    public static boolean activate(PlayerEntity player, ItemStack holdable, Spell spell)
+    {
+        if (spell.needsActivation() && !hasMeta(holdable)) {
+            player.world.playSound(null, player.getPosition(), StrangeSounds.SPELL_CHARGE_SHORT, SoundCategory.PLAYERS, 1.0F, 1.0F);
+            spell.activate(player, holdable);
+            return true;
+        }
+        return false;
+    }
+
+    public static void cast(PlayerEntity player, ItemStack holdable, Spell spell, Consumer<ItemStack> onCastSuccess)
+    {
+        if (player.world.isRemote) return;
+        if (spell == null) return;
+
+        player.world.playSound(null, player.getPosition(), StrangeSounds.SPELL_CAST, SoundCategory.PLAYERS, 1.0F, 1.0F);
+        player.getCooldownTracker().setCooldown(holdable.getItem(), 20);
+
+        spell.cast(player, holdable, result -> {
+            if (result) {
+                clearMeta(holdable);
+                effectEnchant((ServerWorld)player.world, player.getPositionVec(), spell, 5, 0.2D, 0.2D, 0.2, 2.2D);
+                if (!player.isCreative()) {
+                    boolean hasUses = decreaseUses(holdable);
+                }
+                onCastSuccess.accept(holdable);
+            } else {
+                player.world.playSound(null, player.getPosition(), StrangeSounds.SPELL_FAIL, SoundCategory.PLAYERS, 1.0F, 1.0F);
+            }
+        });
+    }
+
+    public static boolean decreaseUses(ItemStack holdable)
+    {
+        CompoundNBT tag = holdable.getOrCreateTag();
+        int remaining = tag.getInt(USES) - 1;
+
+        if (remaining <= 0) {
+            return false;
+        } else {
+            tag.putInt(USES, remaining);
+            return true;
+        }
+    }
+
+    public static void clearMeta(ItemStack holdable)
+    {
+        CompoundNBT holdableTag = holdable.getOrCreateTag();
+        holdableTag.remove(META);
+    }
+
+    public static CompoundNBT getMeta(ItemStack holdable)
+    {
+        return holdable.getOrCreateChildTag(META);
+    }
+
+    public static void putMeta(ItemStack holdable, CompoundNBT tag)
+    {
+        CompoundNBT holdableTag = holdable.getOrCreateTag();
+        holdableTag.put(META, tag);
+    }
+
+    public static boolean hasMeta(ItemStack holdable)
+    {
+        CompoundNBT meta = getMeta(holdable);
+        boolean b = meta.isEmpty();
+        return !b;
+    }
+
+    public static int getUses(ItemStack holdable)
+    {
+        return holdable.getOrCreateTag().getInt(USES);
+    }
+
+    public static void putUses(ItemStack holdable, int uses)
+    {
+        holdable.getOrCreateTag().putInt(USES, uses);
     }
 
     public static void effectEnchant(ServerWorld world, Vec3d vec, Spell spell, int particles, double xOffset, double yOffset, double zOffset, double speed)
