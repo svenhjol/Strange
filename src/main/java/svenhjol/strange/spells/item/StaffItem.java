ac54a47563d6af86cdc17c82c3dde7a917f17056
Sven <sven.hjol@protonmail.com>
2020-01-12 18:33:46 +0000

    Staff now supports efficiency and fortune

--- src/main/java/svenhjol/strange/spells/item/StaffItem.java
+++ src/main/java/svenhjol/strange/spells/item/StaffItem.java
@@ -1,12 +1,17 @@
 package svenhjol.strange.spells.item;
 
+import com.google.common.collect.Sets;
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.enchantment.Enchantments;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.*;
 import net.minecraft.util.*;
 import net.minecraft.world.World;
 import svenhjol.meson.MesonModule;
+import svenhjol.meson.helper.EnchantmentsHelper;
 import svenhjol.meson.iface.IMesonItem;
 import svenhjol.strange.base.StrangeSounds;
 import svenhjol.strange.spells.helper.SpellsHelper;
@@ -16,12 +21,12 @@ import svenhjol.strange.spells.spells.Spell;
 
 import javax.annotation.Nullable;
 
-public class StaffItem extends TieredItem implements IMesonItem
+public class StaffItem extends ToolItem implements IMesonItem
 {
     public StaffItem(MesonModule module, String type, IItemTier tier)
     {
-        super(tier, new Item.Properties()
-            .group(ItemGroup.COMBAT));
+        super(tier.getAttackDamage() + 2.0F, -1.0F, tier, Sets.newHashSet(), new Item.Properties()
+            .group(ItemGroup.TOOLS));
 
         this.register(module, type + "_staff");
     }
@@ -50,10 +55,12 @@ public class StaffItem extends TieredItem implements IMesonItem
         }
 
         player.setActiveHand(hand);
-        player.world.playSound(null, player.getPosition(), StrangeSounds.SPELL_CHARGE_SHORT, SoundCategory.PLAYERS, 1.0F, 1.0F);
+
+        int level = getEfficiency(staff);
+        SoundEvent sound = level < 3 ? StrangeSounds.SPELL_CHARGE_MEDIUM : StrangeSounds.SPELL_CHARGE_SHORT;
+        player.world.playSound(null, player.getPosition(), sound, SoundCategory.PLAYERS, 0.5F, 0.8F + (level * 0.1F));
         return new ActionResult<>(result, staff);
     }
-
 
     @Override
     public UseAction getUseAction(ItemStack staff)
@@ -64,7 +71,8 @@ public class StaffItem extends TieredItem implements IMesonItem
     @Override
     public int getUseDuration(ItemStack staff)
     {
-        return 18;
+        int level = getEfficiency(staff);
+        return 30 - (level * 5);
     }
 
     @Override
@@ -91,9 +99,9 @@ public class StaffItem extends TieredItem implements IMesonItem
             if (entity instanceof PlayerEntity) {
                 PlayerEntity player = (PlayerEntity)entity;
 
-                int cost = spell.getApplyCost();
-                int xp = player.experienceTotal;
-                int levels = player.experienceLevel;
+                final int cost = spell.getApplyCost();
+                final int xp = player.experienceTotal;
+                final int levels = player.experienceLevel;
 
                 if (!player.isCreative()) {
                     if (xp <= 0 || levels - cost < 0) {
@@ -106,19 +114,36 @@ public class StaffItem extends TieredItem implements IMesonItem
                 boolean activated = Spells.activate(player, staff, spell);
                 if (activated) return staff;
 
-                Spells.cast(player, staff, spell, s -> {
-                    // take XP from player
+                Spells.cast(player, staff, spell, used -> {
                     if (!player.isCreative()) {
-                        player.addExperienceLevel(-cost);
+                        int xpCost = cost;
+                        boolean damageBook = true;
 
-                        // damage spell book
+                        int fortune = getFortune(staff);
+                        int efficiency = getEfficiency(staff);
+
+                        if (world.rand.nextFloat() < fortune * 0.2F)
+                            xpCost = Math.min(0, xpCost - fortune);
+
+                        if (world.rand.nextFloat() < efficiency * 0.1F)
+                            damageBook = false;
+
+                        if (xpCost > 0)
+                            player.addExperienceLevel(-xpCost);
+
+                        // damage spell book - efficiency decreases chance
                         ItemStack book = player.getHeldItemOffhand();
-                        if (book.attemptDamageItem(1, world.rand, (ServerPlayerEntity) player)) {
+                        if (damageBook && book.attemptDamageItem(1, world.rand, (ServerPlayerEntity) player)) {
                             book.shrink(1);
+                            player.world.playSound(null, player.getPosition(), StrangeSounds.SPELL_NO_MORE_USES, SoundCategory.PLAYERS, 1.0F, 1.0F);
+                            player.sendBreakAnimation(EquipmentSlotType.OFFHAND);
                         }
 
-                        // damage staff
-                        s.attemptDamageItem(1, world.rand, (ServerPlayerEntity) player);
+                        // damage staff - unbreaking decreases chance
+                        if (staff.attemptDamageItem(1, world.rand, (ServerPlayerEntity) player)) {
+                            staff.shrink(1);
+                            player.sendBreakAnimation(EquipmentSlotType.MAINHAND);
+                        }
                     }
                 });
             }
@@ -141,5 +166,25 @@ public class StaffItem extends TieredItem implements IMesonItem
     public boolean isEnabled()
     {
         return true;
+    }
+
+    public int getEfficiency(ItemStack staff)
+    {
+        int level = 0;
+
+        if (EnchantmentsHelper.hasEnchantment(Enchantments.EFFICIENCY, staff))
+            level = EnchantmentHelper.getEnchantments(staff).get(Enchantments.EFFICIENCY);
+
+        return level;
+    }
+
+    public int getFortune(ItemStack staff)
+    {
+        int level = 0;
+
+        if (EnchantmentsHelper.hasEnchantment(Enchantments.FORTUNE, staff))
+            level = EnchantmentHelper.getEnchantments(staff).get(Enchantments.FORTUNE);
+
+        return level;
     }
 }
