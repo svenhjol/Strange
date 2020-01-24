e55c5ca21f814530ddaffd491fbd6dc8266bba53
Sven <sven.hjol@protonmail.com>
2020-01-24 14:22:24 +0000

    Added a base category

--- src/main/java/svenhjol/strange/base/StrangeCategories.java
+++ src/main/java/svenhjol/strange/base/StrangeCategories.java
@@ -2,11 +2,12 @@ package svenhjol.strange.base;
 
 public class StrangeCategories
 {
+    public static final String BASE = "Base";
     public static final String OUTERLANDS = "Outerlands";
+    public static final String RUINS = "Ruins";
     public static final String RUNESTONES = "Runestones";
     public static final String SCROLLS = "Scrolls";
     public static final String SPELLS = "Spells";
     public static final String TOTEMS = "Totems";
     public static final String TRAVEL_JOURNAL = "TravelJournal";
-    public static final String RUINS = "Ruins";
 }
