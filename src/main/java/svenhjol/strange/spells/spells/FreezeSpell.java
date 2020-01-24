ece569bc024e0052cda02b62467b36773a44b377
Sven <sven.hjol@protonmail.com>
2020-01-24 14:19:29 +0000

    Freeze slows monsters, puts out mobs on fire, and makes pretty snow

--- src/main/java/svenhjol/strange/spells/spells/FreezeSpell.java
+++ src/main/java/svenhjol/strange/spells/spells/FreezeSpell.java
@@ -4,15 +4,24 @@ import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.FireBlock;
+import net.minecraft.block.material.Material;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.monster.MonsterEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.DyeColor;
 import net.minecraft.item.ItemStack;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.potion.Effects;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.util.SoundEvents;
+import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
+import java.util.List;
+import java.util.Objects;
 import java.util.function.Consumer;
+import java.util.function.Predicate;
 
 public class FreezeSpell extends Spell
 {
@@ -26,10 +35,35 @@ public class FreezeSpell extends Spell
     @Override
     public void cast(PlayerEntity player, ItemStack stone, Consumer<Boolean> didCast)
     {
-        this.castArea(player, new int[] { 6, 4, 6 }, blocks -> {
+        int[] range = { 6, 4, 6 };
+        this.castArea(player, range, blocks -> {
             World world = player.world;
-
             if (world.isRemote) return;
+
+            AxisAlignedBB area = player.getBoundingBox().grow(range[0], range[1], range[2]);
+
+            List<MonsterEntity> monsters;
+            Predicate<MonsterEntity> selector = e -> !e.isEntityEqual(player);
+            monsters = world.getEntitiesWithinAABB(MonsterEntity.class, area, selector);
+
+            if (!monsters.isEmpty()) {
+                for (MonsterEntity monster : monsters) {
+                    monster.addPotionEffect(new EffectInstance(Effects.SLOWNESS, 80, 3));
+                }
+            }
+
+            List<LivingEntity> entities;
+            Predicate<LivingEntity> livingSelector = Objects::nonNull;
+            entities = world.getEntitiesWithinAABB(LivingEntity.class, area, livingSelector);
+
+            if (!entities.isEmpty()) {
+                for (LivingEntity entity : entities) {
+                    if (entity.isBurning()) {
+                        entity.extinguish();
+                    }
+                }
+            }
+
             boolean didAnyFreeze = false;
 
             for (BlockPos pos : blocks) {
@@ -55,6 +89,12 @@ public class FreezeSpell extends Spell
                 } else if (state.getBlock() == Blocks.WATER) {
                     world.setBlockState(pos, Blocks.FROSTED_ICE.getDefaultState(), 2);
                     didFreeze = true;
+                } else if (state.isSolid()
+                    && state.getMaterial() != Material.SNOW
+                    && world.isAirBlock(pos.up())
+                    && world.rand.nextFloat() < 0.3F
+                ) {
+                    world.setBlockState(pos.up(), Blocks.SNOW.getDefaultState(), 2);
                 }
 
                 if (didFreeze) {
