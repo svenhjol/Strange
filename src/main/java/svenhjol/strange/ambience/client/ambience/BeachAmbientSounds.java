07aafba1356d989b53e4655e43be24acd016fc23
Sven <sven.hjol@protonmail.com>
2020-02-22 15:24:09 +0000

    Beach sounds now just select from long pool

--- src/main/java/svenhjol/strange/ambience/client/ambience/BeachAmbientSounds.java
+++ src/main/java/svenhjol/strange/ambience/client/ambience/BeachAmbientSounds.java
@@ -5,94 +5,47 @@ import net.minecraft.client.entity.player.ClientPlayerEntity;
 import net.minecraft.util.SoundEvent;
 import net.minecraft.world.biome.Biome;
 import svenhjol.strange.ambience.client.iface.IBiomeAmbience;
-import svenhjol.strange.base.StrangeLoader;
 import svenhjol.strange.base.StrangeSounds;
 
 import javax.annotation.Nullable;
 
-public class BeachAmbientSounds
+public class BeachAmbientSounds extends BaseAmbientSounds implements IBiomeAmbience
 {
-    public static class Day extends BaseAmbientSounds implements IBiomeAmbience
+    public BeachAmbientSounds(ClientPlayerEntity player, SoundHandler soundHandler)
     {
-        public Day(ClientPlayerEntity player, SoundHandler soundHandler)
-        {
-            super(player, soundHandler);
-        }
-
-        @Nullable
-        @Override
-        public SoundEvent getLongSound()
-        {
-            return StrangeSounds.AMBIENCE_BEACH_DAY_LONG;
-        }
-
-        @Nullable
-        @Override
-        public SoundEvent getShortSound()
-        {
-            return null;
-        }
-
-        @Override
-        public int getShortSoundDelay()
-        {
-            return world.rand.nextInt(400) + 400;
-        }
-
-        @Override
-        public float getLongSoundVolume()
-        {
-            return 0.05F;
-        }
-
-        @Override
-        public boolean validBiomeConditions(Biome.Category biomeCategory)
-        {
-            return biomeCategory == Biome.Category.BEACH
-                && StrangeLoader.client.isDaytime
-                && isOutside();
-        }
+        super(player, soundHandler);
     }
 
-    public static class Night extends BaseAmbientSounds implements IBiomeAmbience
+    @Nullable
+    @Override
+    public SoundEvent getLongSound()
     {
-        public Night(ClientPlayerEntity player, SoundHandler soundHandler)
-        {
-            super(player, soundHandler);
-        }
+        return StrangeSounds.AMBIENCE_BEACH_NIGHT_LONG;
+    }
 
-        @Nullable
-        @Override
-        public SoundEvent getLongSound()
-        {
-            return StrangeSounds.AMBIENCE_BEACH_NIGHT_LONG;
-        }
+    @Nullable
+    @Override
+    public SoundEvent getShortSound()
+    {
+        return null;
+    }
 
-        @Nullable
-        @Override
-        public SoundEvent getShortSound()
-        {
-            return null;
-        }
+    @Override
+    public int getShortSoundDelay()
+    {
+        return world.rand.nextInt(400) + 400;
+    }
 
-        @Override
-        public int getShortSoundDelay()
-        {
-            return world.rand.nextInt(400) + 400;
-        }
+    @Override
+    public float getLongSoundVolume()
+    {
+        return 0.05F;
+    }
 
-        @Override
-        public float getLongSoundVolume()
-        {
-            return 0.05F;
-        }
-
-        @Override
-        public boolean validBiomeConditions(Biome.Category biomeCategory)
-        {
-            return biomeCategory == Biome.Category.BEACH
-                && isOutside()
-                && !StrangeLoader.client.isDaytime;
-        }
+    @Override
+    public boolean validBiomeConditions(Biome.Category biomeCategory)
+    {
+        return biomeCategory == Biome.Category.BEACH
+            && isOutside();
     }
 }
