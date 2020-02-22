1508a0debe0337c2b2386ad7c3b87b80c715769d
Sven <sven.hjol@protonmail.com>
2020-02-22 15:24:29 +0000

    Beach sounds now just select from long pool

--- src/main/java/svenhjol/strange/ambience/client/AmbienceHandler.java
+++ src/main/java/svenhjol/strange/ambience/client/AmbienceHandler.java
@@ -24,9 +24,8 @@ public class AmbienceHandler implements IAmbientSoundHandler
             new HighAmbientSounds(player, soundHandler),
             new MineshaftAmbientSounds(player, soundHandler),
             new NetherAmbientSounds(player, soundHandler),
+            new BeachAmbientSounds(player, soundHandler),
 
-            new BeachAmbientSounds.Day(player, soundHandler),
-            new BeachAmbientSounds.Night(player, soundHandler),
             new CaveAmbientSounds.CrystalCaves(player, soundHandler),
             new DesertAmbientSounds.Day(player, soundHandler),
             new DesertAmbientSounds.Night(player, soundHandler),
