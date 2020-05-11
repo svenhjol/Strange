331acedfe86e2ca04954d5813807db92b9b2d64a
Sven <sven.hjol@protonmail.com>
2020-05-11 00:34:50 +0000

    Vaults can now spawn in more biomes

--- src/main/java/svenhjol/strange/ruins/module/Vaults.java
+++ src/main/java/svenhjol/strange/ruins/module/Vaults.java
@@ -67,7 +67,11 @@ public class Vaults extends MesonModule {
         BiomeHelper.getBiomeName(Biomes.MOUNTAINS),
         BiomeHelper.getBiomeName(Biomes.MOUNTAIN_EDGE),
         BiomeHelper.getBiomeName(Biomes.SHATTERED_SAVANNA),
-        BiomeHelper.getBiomeName(Biomes.SHATTERED_SAVANNA_PLATEAU)
+        BiomeHelper.getBiomeName(Biomes.SHATTERED_SAVANNA_PLATEAU),
+        BiomeHelper.getBiomeName(Biomes.PLAINS),
+        BiomeHelper.getBiomeName(Biomes.SUNFLOWER_PLAINS),
+        BiomeHelper.getBiomeName(Biomes.BADLANDS),
+        BiomeHelper.getBiomeName(Biomes.BADLANDS_PLATEAU)
     ));
 
     public static final List<Biome> validBiomes = new ArrayList<>();
