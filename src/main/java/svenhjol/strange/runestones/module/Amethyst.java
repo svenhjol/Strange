05194100b583248bb2b5192e8e0d1549c7875607
Sven <sven.hjol@protonmail.com>
2020-06-02 22:29:23 +0000

    Remaining polished amethyst block types

--- src/main/java/svenhjol/strange/runestones/module/Amethyst.java
+++ src/main/java/svenhjol/strange/runestones/module/Amethyst.java
@@ -4,18 +4,23 @@ import svenhjol.charm.base.CharmCategories;
 import svenhjol.meson.MesonModule;
 import svenhjol.meson.iface.Module;
 import svenhjol.strange.Strange;
-import svenhjol.strange.runestones.block.AmethystBlock;
-import svenhjol.strange.runestones.block.PolishedAmethystBlock;
+import svenhjol.strange.runestones.block.*;
 
 @Module(mod = Strange.MOD_ID, category = CharmCategories.DECORATION)
 public class Amethyst extends MesonModule {
     public static AmethystBlock block;
     public static PolishedAmethystBlock polishedBlock;
+    public static PolishedAmethystSlabBlock polishedSlabBlock;
+    public static PolishedAmethystStairsBlock polishedStairsBlock;
+    public static PolishedAmethystWallBlock polishedWallBlock;
 
     @Override
     public void init() {
         block = new AmethystBlock(this);
         polishedBlock = new PolishedAmethystBlock(this);
+        polishedSlabBlock = new PolishedAmethystSlabBlock(this);
+        polishedStairsBlock = new PolishedAmethystStairsBlock(this);
+        polishedWallBlock = new PolishedAmethystWallBlock(this);
 
         // amethyst rune portal blocks are instantiated by RunePortals module
     }
