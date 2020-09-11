97b60fdc650b3c39a3ab5dcbecfbadc5c93b11c5
Sven Hjól <sven.hjol@protonmail.com>
2020-09-11 21:59:41 +0000

    Add runestone helper

---
+++ src/main/java/svenhjol/strange/helper/RunestoneHelper.java
@@ -0,0 +1,8 @@
+package svenhjol.strange.helper;
+
+import net.minecraft.util.Identifier;
+import svenhjol.strange.Strange;
+
+public class RunestoneHelper {
+    public static final Identifier SPAWN = new Identifier(Strange.MOD_ID, "spawn_point");
+}
