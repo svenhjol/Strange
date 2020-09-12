071d653fda3cfb4750758791379f3ca92b2eabc7
Sven Hjól <sven.hjol@protonmail.com>
2020-09-12 01:56:13 +0000

    Ported rune error

--- src/main/java/svenhjol/strange/helper/RunestoneHelper.java
+++ src/main/java/svenhjol/strange/helper/RunestoneHelper.java
@@ -1,8 +1,27 @@
 package svenhjol.strange.helper;
 
+import net.minecraft.block.Blocks;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraft.world.explosion.Explosion;
 import svenhjol.strange.Strange;
+
+import javax.annotation.Nullable;
 
 public class RunestoneHelper {
     public static final Identifier SPAWN = new Identifier(Strange.MOD_ID, "spawn_point");
+
+    public static void runeError(World world, BlockPos pos, boolean destroyBlock, @Nullable PlayerEntity player) {
+        if (player != null)
+            player.addStatusEffect(new StatusEffectInstance(StatusEffects.NAUSEA, 5 * 20));
+
+        world.createExplosion(null, pos.getX() + 0.5D, pos.getY() + 1.0D, pos.getZ() + 0.5D, 1.25F, Explosion.DestructionType.DESTROY);
+
+        if (destroyBlock)
+            world.setBlockState(pos, Blocks.AIR.getDefaultState());
+    }
 }
