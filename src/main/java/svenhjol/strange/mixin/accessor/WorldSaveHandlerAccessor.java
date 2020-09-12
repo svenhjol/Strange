666d206daf13f57d1aa905e4f8c6f66deb64b333
Sven Hjól <sven.hjol@protonmail.com>
2020-09-12 20:30:09 +0000

    Mixin to handle loading and saving custom player data

---
+++ src/main/java/svenhjol/strange/mixin/accessor/WorldSaveHandlerAccessor.java
@@ -0,0 +1,13 @@
+package svenhjol.strange.mixin.accessor;
+
+import net.minecraft.world.WorldSaveHandler;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+import java.io.File;
+
+@Mixin(WorldSaveHandler.class)
+public interface WorldSaveHandlerAccessor {
+    @Accessor
+    File getPlayerDataDir();
+}
