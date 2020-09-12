94d48d21a82dbc9d470298f9592791a6e912c742
Sven Hjól <sven.hjol@protonmail.com>
2020-09-12 20:30:48 +0000

    Wire it all up

--- src/main/java/svenhjol/strange/block/RunestoneBlock.java
+++ src/main/java/svenhjol/strange/block/RunestoneBlock.java
@@ -1,12 +1,16 @@
 package svenhjol.strange.block;
 
 import net.minecraft.block.AbstractBlock;
-import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.world.BlockView;
 import svenhjol.meson.MesonModule;
-import svenhjol.meson.block.MesonBlock;
+import svenhjol.meson.block.MesonBlockWithEntity;
+import svenhjol.strange.blockentity.RunestoneBlockEntity;
 
-public class RunestoneBlock extends MesonBlock {
+import javax.annotation.Nullable;
+
+public class RunestoneBlock extends MesonBlockWithEntity {
     private final int runeValue;
 
     public RunestoneBlock(MesonModule module, int runeValue) {
@@ -16,5 +20,11 @@ public class RunestoneBlock extends MesonBlock {
 
     public int getRuneValue() {
         return runeValue;
+    }
+
+    @Nullable
+    @Override
+    public BlockEntity createBlockEntity(BlockView world) {
+        return new RunestoneBlockEntity();
     }
 }
--- src/main/java/svenhjol/strange/module/Runestones.java
+++ src/main/java/svenhjol/strange/module/Runestones.java
@@ -2,20 +2,27 @@ package svenhjol.strange.module;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.projectile.thrown.EnderPearlEntity;
 import net.minecraft.entity.projectile.thrown.ThrownEntity;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.SoundCategory;
+import net.minecraft.text.TranslatableText;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.hit.HitResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.world.RaycastContext;
 import net.minecraft.world.World;
 import net.minecraft.world.border.WorldBorder;
 import net.minecraft.world.gen.feature.StructureFeature;
@@ -23,22 +30,34 @@ import svenhjol.meson.Meson;
 import svenhjol.meson.MesonModule;
 import svenhjol.meson.event.PlayerTickCallback;
 import svenhjol.meson.event.ThrownEntityImpactCallback;
+import svenhjol.meson.helper.StringHelper;
 import svenhjol.meson.helper.WorldHelper;
 import svenhjol.meson.iface.Config;
 import svenhjol.meson.iface.Module;
+import svenhjol.strange.Strange;
 import svenhjol.strange.base.StrangeSounds;
 import svenhjol.strange.block.RunestoneBlock;
+import svenhjol.strange.blockentity.RunestoneBlockEntity;
+import svenhjol.strange.event.PlayerLoadDataCallback;
+import svenhjol.strange.event.PlayerSaveDataCallback;
 import svenhjol.strange.helper.RunestoneHelper;
 
 import javax.annotation.Nullable;
+import java.io.File;
 import java.util.*;
 
 @Module(description = "Runestones allow fast travel to points of interest in your world by using an Ender Pearl.")
 public class Runestones extends MesonModule {
-    public static final List<RunestoneBlock> runestones = new ArrayList<>();
+    public static final Identifier ID = new Identifier(Strange.MOD_ID, "runestone");
+    public static final String DISCOVERIES_TAG = "discoveries";
+
+    public static final List<RunestoneBlock> RUNESTONE_BLOCKS = new ArrayList<>();
+    public static BlockEntityType<RunestoneBlockEntity> BLOCK_ENTITY;
+
     public static List<Destination> destinations = new ArrayList<>();
     public static Map<UUID, BlockPos> playerTeleportPos = new HashMap<>();
     public static Map<UUID, Integer> playerTeleportTicks = new HashMap<>();
+    public static Map<UUID, List<Integer>> playerDiscoveries = new HashMap<>();
 
     private static final int numberOfRunes = 26;
 
@@ -66,12 +85,41 @@ public class Runestones extends MesonModule {
     @Override
     public void register() {
         for (int i = 0; i < numberOfRunes; i++) {
-            runestones.add(new RunestoneBlock(this, i));
+            RUNESTONE_BLOCKS.add(new RunestoneBlock(this, i));
         }
+
+        BLOCK_ENTITY = BlockEntityType.Builder.create(RunestoneBlockEntity::new).build(null);
+        Registry.register(Registry.BLOCK_ENTITY_TYPE, ID, BLOCK_ENTITY);
     }
 
     @Override
     public void init() {
+        // write player discovered runes to disk
+        PlayerSaveDataCallback.EVENT.register(((player, playerDataDir) -> {
+            UUID uid = player.getUuid();
+            if (playerDiscoveries.containsKey(uid)) {
+                List<Integer> runes = playerDiscoveries.get(uid);
+                CompoundTag tag = new CompoundTag();
+                tag.putIntArray(DISCOVERIES_TAG, runes);
+                PlayerSaveDataCallback.writeFile(new File(playerDataDir, player.getUuidAsString() + "_runestones.dat"), tag);
+            }
+        }));
+
+        PlayerLoadDataCallback.EVENT.register(((player, playerDataDir) -> {
+            UUID uid = player.getUuid();
+            CompoundTag tag = PlayerLoadDataCallback.readFile(new File(playerDataDir, player.getUuidAsString() + "_runestones.dat"));
+            if (tag.contains(DISCOVERIES_TAG)) {
+                playerDiscoveries.remove(uid);
+                playerDiscoveries.put(uid, new ArrayList<>());
+
+                int[] intArray = tag.getIntArray(DISCOVERIES_TAG);
+                for (int i : intArray) {
+                    playerDiscoveries.get(uid).add(i);
+                }
+                Meson.LOG.debug("Loaded player rune discovery data");
+            }
+        }));
+
         // listen for thrown enderpearls
         ThrownEntityImpactCallback.EVENT.register(this::tryEnderPearlImpact);
 
@@ -80,7 +128,7 @@ public class Runestones extends MesonModule {
             if (player.world.isClient || destinations.isEmpty())
                 return;
 
-            // TODO check player looking at the runestone
+            tryLookingAtRunestone((ServerPlayerEntity)player);
             tryTeleport((ServerPlayerEntity)player);
         });
 
@@ -124,6 +172,41 @@ public class Runestones extends MesonModule {
         Random random = new Random();
         random.setSeed(seed);
         Collections.shuffle(destinations, random);
+    }
+
+    private void tryLookingAtRunestone(ServerPlayerEntity player) {
+        // don't check this every single tick
+        if (player.world.getTime() % 10 > 0)
+            return;
+
+        Vec3d cameraPosVec = player.getCameraPosVec(1.0F);
+        Vec3d rotationVec = player.getRotationVec(1.0F);
+        Vec3d vec3d = cameraPosVec.add(rotationVec.x * 6, rotationVec.y * 6, rotationVec.z * 6);
+
+        World world = player.world;
+        BlockHitResult raycast = world.raycast(new RaycastContext(cameraPosVec, vec3d, RaycastContext.ShapeType.OUTLINE, RaycastContext.FluidHandling.NONE, player));
+        BlockPos runePos = raycast.getBlockPos();
+        BlockState lookedAt = world.getBlockState(runePos);
+
+        if (lookedAt.getBlock() instanceof RunestoneBlock) {
+            RunestoneBlock block = (RunestoneBlock)lookedAt.getBlock();
+            if (RUNESTONE_BLOCKS.contains(block)) {
+                TranslatableText message = new TranslatableText("runestone.strange.unknown");
+                int runeValue = getRuneValue((ServerWorld) world, runePos);
+
+                // TODO player knowledge of the rune
+
+                BlockEntity blockEntity = world.getBlockEntity(runePos);
+                if (blockEntity instanceof RunestoneBlockEntity) {
+                    RunestoneBlockEntity runestone = (RunestoneBlockEntity)blockEntity;
+
+                    if (runestone.description != null)
+                        message = new TranslatableText("runestone.strange.discovered", runestone.description);
+                }
+
+                player.sendMessage(message, true);
+            }
+        }
     }
 
     private void tryTeleport(ServerPlayerEntity player) {
@@ -223,11 +306,17 @@ public class Runestones extends MesonModule {
             return RunestoneHelper.explode(world, runePos, player, true);
         }
 
-        // TODO research how to store playerdata in fabric! Need to learn rune here
+        UUID uid = player.getUuid();
+
+        if (!playerDiscoveries.containsKey(uid))
+            playerDiscoveries.put(uid, new ArrayList<>());
+
+        if (!playerDiscoveries.get(uid).contains(runeValue))
+            playerDiscoveries.get(uid).add(runeValue);
 
         // prep for teleport
-        playerTeleportPos.put(player.getUuid(), destPos);
-        playerTeleportTicks.put(player.getUuid(), 10);
+        playerTeleportPos.put(uid, destPos);
+        playerTeleportTicks.put(uid, 10);
 
         return true;
     }
@@ -255,7 +344,17 @@ public class Runestones extends MesonModule {
         public BlockPos getDestination(ServerWorld world, BlockPos runePos, Random random) {
             int maxDistance = Runestones.maxDistance;
 
-            // TODO block entity fetch destination
+            BlockEntity blockEntity = world.getBlockEntity(runePos);
+            if (blockEntity instanceof RunestoneBlockEntity) {
+                RunestoneBlockEntity runeBlockEntity = (RunestoneBlockEntity)blockEntity;
+                BlockPos position = runeBlockEntity.position;
+                String structure = runeBlockEntity.structure;
+
+                if (structure != null && !structure.isEmpty() && position != null) {
+                    Meson.LOG.debug("Found destination in runestone: " + structure);
+                    return position;
+                }
+            }
 
             // bounds check
             WorldBorder border = world.getWorldBorder();
@@ -301,7 +400,15 @@ public class Runestones extends MesonModule {
 
             foundPos = RunestoneHelper.addRandomOffset(foundPos, random, 8);
 
-            // TODO block entity store destination
+            if (blockEntity instanceof RunestoneBlockEntity) {
+                RunestoneBlockEntity runeBlockEntity = (RunestoneBlockEntity)blockEntity;
+                runeBlockEntity.position = foundPos;
+                runeBlockEntity.structure = this.structure.toString();
+                runeBlockEntity.description = StringHelper.capitalize(this.structure.getPath().replaceAll("_", " "));
+                runeBlockEntity.markDirty();
+
+                Meson.LOG.debug("Stored structure in runestone for next use");
+            }
 
             return foundPos;
         }
