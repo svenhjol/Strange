9a29b3696f6f9bed1efda986f673c56da55e0b81
Sven Hjól <sven.hjol@protonmail.com>
2020-09-15 23:33:59 +0000

    Abstract ruin type for easier definitions

---
+++ src/main/java/svenhjol/strange/ruin/BaseRuin.java
@@ -0,0 +1,55 @@
+package svenhjol.strange.ruin;
+
+import com.google.common.collect.ImmutableList;
+import com.mojang.datafixers.util.Pair;
+import net.minecraft.structure.pool.StructurePool;
+import net.minecraft.structure.pool.StructurePoolElement;
+import net.minecraft.structure.pool.StructurePools;
+import net.minecraft.structure.processor.StructureProcessorLists;
+import net.minecraft.util.Identifier;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+
+public abstract class BaseRuin {
+    private final String modId;
+    private final String ruinName;
+    private final List<Pair<Function<StructurePool.Projection, ? extends StructurePoolElement>, Integer>> starts = new ArrayList<>();
+
+    public BaseRuin(String modId, String ruinName) {
+        this.modId = modId;
+        this.ruinName = ruinName;
+    }
+
+    public List<Pair<Function<StructurePool.Projection, ? extends StructurePoolElement>, Integer>> getStarts() {
+        return starts;
+    }
+
+    protected void addStart(String pieceName, int weight) {
+        starts.add(Pair.of(StructurePoolElement.method_30435(getPiecePath(pieceName), StructureProcessorLists.EMPTY), weight));
+    }
+
+    protected void registerPool(String poolName, Map<String, Integer> elements) {
+        final List<Pair<Function<StructurePool.Projection, ? extends StructurePoolElement>, Integer>> pieces = new ArrayList<>();
+
+        elements.forEach((piece, weight) ->
+            pieces.add(Pair.of(StructurePoolElement.method_30435(getPiecePath(piece), StructureProcessorLists.EMPTY), weight)));
+
+        StructurePools.register(new StructurePool(
+            getPoolPath(poolName),
+            getPoolPath("ends"),
+            ImmutableList.copyOf(pieces),
+            StructurePool.Projection.RIGID
+        ));
+    }
+
+    protected String getPiecePath(String piece) {
+        return modId + ":ruins/" + ruinName + "/" + piece;
+    }
+
+    protected Identifier getPoolPath(String pool) {
+        return new Identifier(modId, "ruins/" + ruinName + "/" + pool);
+    }
+}
