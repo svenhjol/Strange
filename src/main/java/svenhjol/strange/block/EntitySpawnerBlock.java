8414cf1289fcf84885abfde1461619919e0540e9
Sven Hjól <sven.hjol@protonmail.com>
2020-09-16 02:03:17 +0000

    EntitySpawnerBlock is now properly invisible

--- src/main/java/svenhjol/strange/block/EntitySpawnerBlock.java
+++ src/main/java/svenhjol/strange/block/EntitySpawnerBlock.java
@@ -5,6 +5,9 @@ import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.collection.DefaultedList;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
 import svenhjol.meson.MesonModule;
 import svenhjol.meson.block.MesonBlockWithEntity;
@@ -15,8 +18,8 @@ import javax.annotation.Nullable;
 public class EntitySpawnerBlock extends MesonBlockWithEntity {
     public EntitySpawnerBlock(MesonModule module) {
         super(module, "entity_spawner", AbstractBlock.Settings
-            .of(Material.AIR, MaterialColor.BLACK)
-            .strength(-1.0F, 3400000.0F)
+            .of(Material.AIR)
+            .noCollision()
             .dropsNothing());
     }
 
@@ -29,5 +32,13 @@ public class EntitySpawnerBlock extends MesonBlockWithEntity {
     @Override
     public void addStacksForDisplay(ItemGroup group, DefaultedList<ItemStack> list) {
         // don't
+    }
+
+    public BlockRenderType getRenderType(BlockState state) {
+        return BlockRenderType.INVISIBLE;
+    }
+
+    public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+        return VoxelShapes.empty();
     }
 }
