d9c7fd83eb686db5b10d1c47cfed258d0a5a7d14
Sven Hjól <sven.hjol@protonmail.com>
2020-09-16 01:28:36 +0000

    Registrations for all ruin categories

--- src/main/java/svenhjol/strange/module/Ruins.java
+++ src/main/java/svenhjol/strange/module/Ruins.java
@@ -3,7 +3,6 @@ package svenhjol.strange.module;
 import net.fabricmc.fabric.api.structure.v1.FabricStructureBuilder;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.BuiltinRegistries;
-import net.minecraft.world.biome.Biome;
 import net.minecraft.world.gen.GenerationStep;
 import net.minecraft.world.gen.feature.ConfiguredStructureFeature;
 import net.minecraft.world.gen.feature.StructureFeature;
@@ -13,54 +12,96 @@ import svenhjol.meson.helper.BiomeHelper;
 import svenhjol.meson.iface.Config;
 import svenhjol.meson.iface.Module;
 import svenhjol.strange.Strange;
+import svenhjol.strange.ruin.BambiMountainsRuin;
 import svenhjol.strange.structure.RuinFeature;
 import svenhjol.strange.structure.RuinGenerator;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
 
-@Module(description = "Ruins that spawn underground with different types according to biome.")
+import static svenhjol.strange.structure.RuinGenerator.*;
+
+@Module(description = "Underground ruins with different themes according to the biome.")
 public class Ruins extends MesonModule {
     public static final Identifier STRUCTURE_ID = new Identifier(Strange.MOD_ID, "ruin");
 
-    public static StructureFeature<StructurePoolFeatureConfig> RUIN_STRUCTURE;
-    public static ConfiguredStructureFeature<?, ?> RUIN;
+    public static StructureFeature<StructurePoolFeatureConfig> STRUCTURE;
 
-    @Config(name = "Available biomes", description = "Biomes that ruins may generate in.")
-    public static List<String> configBiomes = new ArrayList<>(Arrays.asList(
-        "minecraft:plains",
-        "minecraft:desert",
-        "minecraft:savanna",
-        "minecraft:swamp",
-        "minecraft:sunflower_plains",
-        "minecraft:flower_forest",
-        "minecraft:snowy_tundra"
-    ));
+    public static ConfiguredStructureFeature<?, ?> BADLANDS;
+    public static ConfiguredStructureFeature<?, ?> DESERT;
+    public static ConfiguredStructureFeature<?, ?> FOREST;
+    public static ConfiguredStructureFeature<?, ?> JUNGLE;
+    public static ConfiguredStructureFeature<?, ?> MOUNTAINS;
+    public static ConfiguredStructureFeature<?, ?> NETHER;
+    public static ConfiguredStructureFeature<?, ?> PLAINS;
+    public static ConfiguredStructureFeature<?, ?> SAVANNA;
+    public static ConfiguredStructureFeature<?, ?> SNOWY;
+    public static ConfiguredStructureFeature<?, ?> TAIGA;
+
+    @Config(name = "Ruin size", description = "Size of the generated ruins. For reference, villages are 6.")
+    public static int ruinSize = 6;
 
     @Override
     public void register() {
-        RUIN_STRUCTURE = new RuinFeature(StructurePoolFeatureConfig.CODEC);
-        RUIN = RUIN_STRUCTURE.configure(new StructurePoolFeatureConfig(() -> {
-            return RuinGenerator.pools.get(0);
-        }, 6));
+        STRUCTURE = new RuinFeature(StructurePoolFeatureConfig.CODEC);
 
-        FabricStructureBuilder.create(STRUCTURE_ID, RUIN_STRUCTURE)
+        // register structure so that `strange:ruin` is a valid location and gen spacing is correct
+        FabricStructureBuilder.create(STRUCTURE_ID, STRUCTURE)
             .step(GenerationStep.Feature.UNDERGROUND_STRUCTURES)
             .defaultConfig(24, 8, 12125)
             .register();
 
-        BuiltinRegistries.add(BuiltinRegistries.CONFIGURED_STRUCTURE_FEATURE, STRUCTURE_ID, RUIN);
+        // create configuredFeature objects for each jigsaw pool
+        BADLANDS    = STRUCTURE.configure(new StructurePoolFeatureConfig(() -> BADLANDS_POOL, ruinSize));
+        DESERT      = STRUCTURE.configure(new StructurePoolFeatureConfig(() -> DESERT_POOL, ruinSize));
+        FOREST      = STRUCTURE.configure(new StructurePoolFeatureConfig(() -> FOREST_POOL, ruinSize));
+        JUNGLE      = STRUCTURE.configure(new StructurePoolFeatureConfig(() -> JUNGLE_POOL, ruinSize));
+        MOUNTAINS   = STRUCTURE.configure(new StructurePoolFeatureConfig(() -> MOUNTAINS_POOL, ruinSize));
+        NETHER      = STRUCTURE.configure(new StructurePoolFeatureConfig(() -> NETHER_POOL, ruinSize));
+        PLAINS      = STRUCTURE.configure(new StructurePoolFeatureConfig(() -> PLAINS_POOL, ruinSize));
+        SAVANNA     = STRUCTURE.configure(new StructurePoolFeatureConfig(() -> SAVANNA_POOL, ruinSize));
+        SNOWY       = STRUCTURE.configure(new StructurePoolFeatureConfig(() -> SNOWY_POOL, ruinSize));
+        TAIGA       = STRUCTURE.configure(new StructurePoolFeatureConfig(() -> TAIGA_POOL, ruinSize));
+
+        // register each configuredFeature with MC registry against the RUIN_STRUCTURE
+        registerConfiguredFeature("ruin_badlands", BADLANDS);
+        registerConfiguredFeature("ruin_desert", DESERT);
+        registerConfiguredFeature("ruin_forest", FOREST);
+        registerConfiguredFeature("ruin_jungle", JUNGLE);
+        registerConfiguredFeature("ruin_mountains", MOUNTAINS);
+        registerConfiguredFeature("ruin_nether", NETHER);
+        registerConfiguredFeature("ruin_plains", PLAINS);
+        registerConfiguredFeature("ruin_savanna", SAVANNA);
+        registerConfiguredFeature("ruin_snowy", SNOWY);
+        registerConfiguredFeature("ruin_taiga", TAIGA);
     }
 
     @Override
     public void init() {
+        // register all custom ruins here
+        MOUNTAINS_RUINS.add(new BambiMountainsRuin());
+
+        // builds and registers all custom ruins into pools
         RuinGenerator.init();
 
-        configBiomes.forEach(biomeId -> {
-            Optional<Biome> biome = BuiltinRegistries.BIOME.getOrEmpty(new Identifier(biomeId));
-            biome.ifPresent(value -> BiomeHelper.addStructureFeature(value, RUIN));
-        });
+        // add registered ruin pools to biomes
+        if (!BADLANDS_RUINS.isEmpty()) addToBiome(BiomeHelper.BADLANDS, BADLANDS);
+        if (!DESERT_RUINS.isEmpty()) addToBiome(BiomeHelper.DESERT, DESERT);
+        if (!FOREST_RUINS.isEmpty()) addToBiome(BiomeHelper.FOREST, FOREST);
+        if (!JUNGLE_RUINS.isEmpty()) addToBiome(BiomeHelper.JUNGLE, JUNGLE);
+        if (!MOUNTAINS_RUINS.isEmpty()) addToBiome(BiomeHelper.MOUNTAINS, MOUNTAINS);
+        if (!NETHER_RUINS.isEmpty()) addToBiome(BiomeHelper.NETHER, NETHER);
+        if (!PLAINS_RUINS.isEmpty()) addToBiome(BiomeHelper.PLAINS, PLAINS);
+        if (!SAVANNA_RUINS.isEmpty()) addToBiome(BiomeHelper.SAVANNA, SAVANNA);
+        if (!SNOWY_RUINS.isEmpty()) addToBiome(BiomeHelper.SNOWY, SNOWY);
+        if (!TAIGA_RUINS.isEmpty()) addToBiome(BiomeHelper.TAIGA, TAIGA);
+    }
+
+    private void registerConfiguredFeature(String id, ConfiguredStructureFeature<?, ?> configuredFeature) {
+        BuiltinRegistries.add(BuiltinRegistries.CONFIGURED_STRUCTURE_FEATURE, new Identifier(Strange.MOD_ID, id), configuredFeature);
+    }
+
+    private void addToBiome(List<String> biomeGroup, ConfiguredStructureFeature<?, ?> configuredFeature) {
+        biomeGroup.forEach(id -> BuiltinRegistries.BIOME.getOrEmpty(new Identifier(id))
+            .ifPresent(biome -> BiomeHelper.addStructureFeature(biome, configuredFeature)));
     }
 }
--- src/main/java/svenhjol/strange/structure/RuinGenerator.java
+++ src/main/java/svenhjol/strange/structure/RuinGenerator.java
@@ -1,16 +1,83 @@
 package svenhjol.strange.structure;
 
+import com.google.common.collect.ImmutableList;
+import com.mojang.datafixers.util.Pair;
 import net.minecraft.structure.pool.StructurePool;
-import svenhjol.strange.ruin.BambiMountainsRuin;
+import net.minecraft.structure.pool.StructurePoolElement;
+import net.minecraft.structure.pool.StructurePools;
+import net.minecraft.util.Identifier;
+import svenhjol.strange.Strange;
+import svenhjol.strange.ruin.BaseRuin;
 
+import javax.annotation.Nullable;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
 
 public class RuinGenerator {
-    public static List<StructurePool> pools = new ArrayList<>();
+    public static StructurePool BADLANDS_POOL;
+    public static StructurePool DESERT_POOL;
+    public static StructurePool FOREST_POOL;
+    public static StructurePool JUNGLE_POOL;
+    public static StructurePool MOUNTAINS_POOL;
+    public static StructurePool NETHER_POOL;
+    public static StructurePool PLAINS_POOL;
+    public static StructurePool SAVANNA_POOL;
+    public static StructurePool SNOWY_POOL;
+    public static StructurePool TAIGA_POOL;
+
+    public static List<BaseRuin> BADLANDS_RUINS = new ArrayList<>();
+    public static List<BaseRuin> DESERT_RUINS = new ArrayList<>();
+    public static List<BaseRuin> FOREST_RUINS = new ArrayList<>();
+    public static List<BaseRuin> JUNGLE_RUINS = new ArrayList<>();
+    public static List<BaseRuin> MOUNTAINS_RUINS = new ArrayList<>();
+    public static List<BaseRuin> NETHER_RUINS = new ArrayList<>();
+    public static List<BaseRuin> PLAINS_RUINS = new ArrayList<>();
+    public static List<BaseRuin> SAVANNA_RUINS = new ArrayList<>();
+    public static List<BaseRuin> SNOWY_RUINS = new ArrayList<>();
+    public static List<BaseRuin> TAIGA_RUINS = new ArrayList<>();
 
     public static void init() {
-        BambiMountainsRuin.init();
-        pools.add(BambiMountainsRuin.STARTS);
+        BADLANDS_POOL = registerPool("ruins/badlands/starts", BADLANDS_RUINS);
+        DESERT_POOL = registerPool("ruins/desert/starts", DESERT_RUINS);
+        FOREST_POOL = registerPool("ruins/forest/starts", FOREST_RUINS);
+        JUNGLE_POOL = registerPool("ruins/jungle/starts", JUNGLE_RUINS);
+        MOUNTAINS_POOL = registerPool("ruins/mountains/starts", MOUNTAINS_RUINS);
+        NETHER_POOL = registerPool("ruins/nether/starts", NETHER_RUINS);
+        PLAINS_POOL = registerPool("ruins/plains/starts", PLAINS_RUINS);
+        SAVANNA_POOL = registerPool("ruins/savanna/starts", SAVANNA_RUINS);
+        SNOWY_POOL = registerPool("ruins/snowy/starts", SNOWY_RUINS);
+        TAIGA_POOL = registerPool("ruins/taiga/starts", TAIGA_RUINS);
+    }
+
+    @Nullable
+    private static StructurePool registerPool(String startPool, List<BaseRuin> ruins) {
+        if (ruins.isEmpty())
+            return emptyPool(startPool);
+
+        // this is populated with starts for each custom ruin
+        List<Pair<Function<StructurePool.Projection, ? extends StructurePoolElement>, Integer>> starts = new ArrayList<>();
+
+        // iterate over each custom ruin, get all the start pools, and put them into the starts list
+        ruins.forEach(ruin -> starts.addAll(ruin.getStarts()));
+
+        // return the start pool containing all the custom ruin starts
+        return StructurePools.register(
+            new StructurePool(
+                new Identifier(Strange.MOD_ID, startPool),
+                new Identifier("empty"),
+                ImmutableList.copyOf(starts),
+                StructurePool.Projection.RIGID
+            )
+        );
+    }
+
+    private static StructurePool emptyPool(String poolName) {
+        return new StructurePool(
+            new Identifier(Strange.MOD_ID, poolName),
+            new Identifier("empty"),
+            ImmutableList.of(Pair.of(StructurePoolElement.method_30438(), 1)),
+            StructurePool.Projection.RIGID
+        );
     }
 }
