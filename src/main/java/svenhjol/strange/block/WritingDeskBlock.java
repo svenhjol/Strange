5a55ffa1fd0eafee9291db4d2757b45c233c88d9
Sven Hjól <sven.hjol@protonmail.com>
2020-09-16 21:11:18 +0000

    Update writing desk block render model

--- src/main/java/svenhjol/strange/block/WritingDeskBlock.java
+++ src/main/java/svenhjol/strange/block/WritingDeskBlock.java
@@ -5,6 +5,8 @@ import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.DirectionProperty;
+import net.minecraft.util.BlockMirror;
+import net.minecraft.util.BlockRotation;
 import svenhjol.meson.MesonModule;
 import svenhjol.meson.block.MesonBlock;
 import svenhjol.strange.module.Scrollkeepers;
@@ -18,10 +20,22 @@ public class WritingDeskBlock extends MesonBlock {
         super(module, Scrollkeepers.BLOCK_ID.getPath(), AbstractBlock.Settings.copy(Blocks.CARTOGRAPHY_TABLE));
     }
 
+    public BlockRenderType getRenderType(BlockState state) {
+        return BlockRenderType.MODEL;
+    }
+
     @Nullable
     @Override
     public BlockState getPlacementState(ItemPlacementContext ctx) {
         return getDefaultState().with(FACING, ctx.getPlayerLookDirection().getOpposite());
+    }
+
+    public BlockState rotate(BlockState state, BlockRotation rotation) {
+        return state.with(FACING, rotation.rotate(state.get(FACING)));
+    }
+
+    public BlockState mirror(BlockState state, BlockMirror mirror) {
+        return state.rotate(mirror.getRotation(state.get(FACING)));
     }
 
     @Override
