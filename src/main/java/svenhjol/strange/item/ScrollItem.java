02a1bc9eef2b1a16136216dab198d9976d6df052
Sven Hjól <sven.hjol@protonmail.com>
2020-09-24 15:52:28 +0000

    Implementation of scroll use WIP

--- src/main/java/svenhjol/strange/item/ScrollItem.java
+++ src/main/java/svenhjol/strange/item/ScrollItem.java
@@ -4,15 +4,19 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.Hand;
-import net.minecraft.util.Rarity;
-import net.minecraft.util.TypedActionResult;
+import net.minecraft.text.TranslatableText;
+import net.minecraft.util.*;
 import net.minecraft.world.World;
 import svenhjol.meson.MesonModule;
+import svenhjol.meson.helper.DimensionHelper;
 import svenhjol.meson.item.MesonItem;
+import svenhjol.strange.module.Scrolls;
+import svenhjol.strange.scroll.ScrollDefinition;
+import svenhjol.strange.scroll.ScrollQuest;
+import svenhjol.strange.scroll.ScrollQuestCreator;
 
 public class ScrollItem extends MesonItem {
-    private static final String DEFINITION_TAG = "definition";
+    private static final String QUEST_TAG = "quest";
     private static final String RARITY_TAG = "rarity";
 
     private final int tier;
@@ -29,6 +33,48 @@ public class ScrollItem extends MesonItem {
 
     @Override
     public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
+        ItemStack scrollItem = user.getStackInHand(hand);
+        int rarity = getScrollRarity(scrollItem);
+
+        if (!DimensionHelper.isDimension(world, new Identifier("overworld")) || user.isSneaking())
+            return new TypedActionResult<>(ActionResult.FAIL, scrollItem);
+
+        user.getItemCooldownManager().set(this, 40);
+
+        if (world.isClient)
+            return new TypedActionResult<>(ActionResult.PASS, scrollItem);
+
+        if (!hasBeenPopulated(scrollItem)) {
+            ScrollDefinition definition = Scrolls.getRandomDefinition(tier, world.random);
+
+            if (definition == null)
+                return new TypedActionResult<>(ActionResult.FAIL, scrollItem);
+
+            ScrollQuest quest = ScrollQuestCreator.create(user, definition, rarity);
+            setScrollQuest(scrollItem, quest);
+            scrollItem.setCustomName(new TranslatableText(quest.getTitle()));
+
+            // TODO: send message to client to open scroll
+
+            return new TypedActionResult<>(ActionResult.SUCCESS, scrollItem);
+        }
+
         return super.use(world, user, hand);
+    }
+
+    public static boolean hasBeenPopulated(ItemStack scroll) {
+        return scroll.getOrCreateTag().contains(QUEST_TAG);
+    }
+
+    public static int getScrollRarity(ItemStack scroll) {
+        return scroll.getOrCreateTag().getInt(RARITY_TAG);
+    }
+
+    public static void setScrollRarity(ItemStack scroll, int rarity) {
+        scroll.getOrCreateTag().putInt(RARITY_TAG, rarity);
+    }
+
+    public static void setScrollQuest(ItemStack scroll, ScrollQuest quest) {
+        scroll.getOrCreateTag().put(QUEST_TAG, quest.toTag());
     }
 }
