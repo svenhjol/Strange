ae13ecfdd8eaf6428647e46f4cf2ae211e8e9443
Sven Hjól <sven.hjol@protonmail.com>
2020-09-25 01:17:58 +0000

    Register AmethystStairsBlock

--- src/main/java/svenhjol/strange/module/Amethyst.java
+++ src/main/java/svenhjol/strange/module/Amethyst.java
@@ -1,15 +1,19 @@
 package svenhjol.strange.module;
 
+import net.minecraft.block.StairsBlock;
 import svenhjol.meson.MesonModule;
 import svenhjol.meson.iface.Module;
 import svenhjol.strange.block.AmethystBlock;
+import svenhjol.strange.block.AmethystStairsBlock;
 
 @Module(description = "Decorative block found in the End Islands.")
 public class Amethyst extends MesonModule {
     public static AmethystBlock AMETHYST;
+    public static StairsBlock AMETHYST_STAIRS;
 
     @Override
     public void register() {
         AMETHYST = new AmethystBlock(this);
+        AMETHYST_STAIRS = new AmethystStairsBlock(this);
     }
 }
