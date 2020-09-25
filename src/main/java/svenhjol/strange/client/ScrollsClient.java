18baf3fc80b8e20ce5bfc287bb0bc7e67f81ac8e
Sven Hjól <sven.hjol@protonmail.com>
2020-09-25 15:28:52 +0000

    ScrollsClient to handle client-side events

---
+++ src/main/java/svenhjol/strange/client/ScrollsClient.java
@@ -0,0 +1,35 @@
+package svenhjol.strange.client;
+
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.fabricmc.fabric.api.network.ClientSidePacketRegistry;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.nbt.CompoundTag;
+import svenhjol.meson.MesonModule;
+import svenhjol.strange.gui.ScrollScreen;
+import svenhjol.strange.scroll.ScrollQuest;
+
+import static svenhjol.strange.module.Scrolls.MSG_CLIENT_OPEN_SCROLL;
+
+@Environment(EnvType.CLIENT)
+public class ScrollsClient {
+    private final MesonModule module;
+
+    public ScrollsClient(MesonModule module) {
+        this.module = module;
+
+        // listen for scroll open events being sent from the server
+        ClientSidePacketRegistry.INSTANCE.register(MSG_CLIENT_OPEN_SCROLL, (context, data) -> {
+            CompoundTag questTag = data.readCompoundTag();
+            context.getTaskQueue().execute(() -> {
+                tryReadQuest(questTag);
+            });
+        });
+    }
+
+    private void tryReadQuest(CompoundTag questTag) {
+        ScrollQuest quest = new ScrollQuest();
+        quest.fromTag(questTag);
+        MinecraftClient.getInstance().openScreen(new ScrollScreen(quest));
+    }
+}
