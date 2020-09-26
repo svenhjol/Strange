4bc63bd329e911c4d65b66482c0ce6d1c0e7f484
Sven Hjól <sven.hjol@protonmail.com>
2020-09-26 01:44:59 +0000

    Added common values to BasePanel

--- src/main/java/svenhjol/strange/scroll/BasePanel.java
+++ src/main/java/svenhjol/strange/scroll/BasePanel.java
@@ -6,5 +6,9 @@ import svenhjol.strange.helper.GuiHelper;
 import svenhjol.strange.scroll.tag.QuestTag;
 
 public abstract class BasePanel extends GuiHelper {
+    protected int textColor = 0xFFFFFF;
+    protected int titleColor = 0xFFFF00;
+    protected int rowHeight = 16;
+
     public abstract void render(Screen screen, MatrixStack matrices, QuestTag quest, int mid, int width, int top, int mouseX, int mouseY);
 }
