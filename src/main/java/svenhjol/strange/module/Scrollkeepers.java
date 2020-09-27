8d984ab02726a65c7b0ae6bceac3aea737ee678e
Sven Hjól <sven.hjol@protonmail.com>
2020-09-27 02:02:43 +0000

    Villagers now level up on quest complete

--- src/main/java/svenhjol/strange/module/Scrollkeepers.java
+++ src/main/java/svenhjol/strange/module/Scrollkeepers.java
@@ -3,6 +3,8 @@ package svenhjol.strange.module;
 import net.fabricmc.fabric.api.event.player.UseEntityCallback;
 import net.minecraft.client.render.RenderLayer;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.passive.VillagerEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
@@ -12,9 +14,11 @@ import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.hit.EntityHitResult;
+import net.minecraft.village.VillagerData;
 import net.minecraft.village.VillagerProfession;
 import net.minecraft.world.World;
 import net.minecraft.world.poi.PointOfInterestType;
+import svenhjol.meson.Meson;
 import svenhjol.meson.MesonModule;
 import svenhjol.meson.event.PlayerTickCallback;
 import svenhjol.meson.helper.VillagerHelper;
@@ -33,6 +37,7 @@ import svenhjol.strange.village.ScrollkeeperTradeOffers.ScrollForEmeralds;
 public class Scrollkeepers extends MesonModule {
     public static Identifier BLOCK_ID = new Identifier(Strange.MOD_ID, "writing_desk");
     public static Identifier VILLAGER_ID = new Identifier(Strange.MOD_ID, "scrollkeeper");
+    public static final int[] QUEST_XP = new int[]{1, 10, 16, 24, 35};
 
     public static WritingDeskBlock WRITING_DESK;
     public static VillagerProfession SCROLLKEEPER;
@@ -41,6 +46,7 @@ public class Scrollkeepers extends MesonModule {
     public static ScrollKeepersClient client;
 
     public static int interestRange = 16;
+    public static double badOmenChance = 0.03D;
 
     @Override
     public void register() {
@@ -94,7 +100,7 @@ public class Scrollkeepers extends MesonModule {
 
                 // must be the merchant you bought the scroll from, or a scroll you found
                 if (!villager.getUuid().equals(quest.getMerchant())
-                    || quest.getMerchant().equals(ScrollHelper.ANY_MERCHANT)) {
+                    && !quest.getMerchant().equals(ScrollHelper.ANY_MERCHANT)) {
                     ((VillagerEntityAccessor)villager).invokeSayNo();
                     return ActionResult.FAIL;
                 }
@@ -104,6 +110,35 @@ public class Scrollkeepers extends MesonModule {
                 world.playSound(null, playerEntity.getBlockPos(), SoundEvents.UI_TOAST_CHALLENGE_COMPLETE, SoundCategory.PLAYERS, 1.0F, 1.0F);
                 quest.complete(playerEntity, villager);
                 heldStack.decrement(1);
+
+                // handle villager xp increase and level-up
+                VillagerData villagerData = villager.getVillagerData();
+                int villagerXp = villager.getExperience();
+                int villagerLevel = villagerData.getLevel();
+                int questTier = quest.getTier();
+                int questRarity = quest.getRarity();
+
+                if (questTier >= villagerLevel) {
+                    int tierXp = QUEST_XP[questTier - 1];
+                    if (questTier > villagerLevel)
+                        tierXp /= 2;
+
+                    int rareXp = tierXp * questRarity;
+                    int newVillagerXp = villagerXp + Math.max(tierXp, rareXp);
+                    villager.setCurrentCustomer(null);
+                    villager.setExperience(newVillagerXp);
+
+                    if (((VillagerEntityAccessor)villager).invokeCanLevelUp())
+                        ((VillagerEntityAccessor)villager).invokeLevelUp();
+                }
+
+                // handle bad omen penalty
+                if (badOmenChance > 0 && villagerLevel >= 3 && world.random.nextFloat() < (badOmenChance * (villagerLevel - 2))) {
+                    int amplifier = Math.max(0, villagerLevel - 2);
+                    StatusEffectInstance badOmen = new StatusEffectInstance(StatusEffects.BAD_OMEN, 120000, amplifier, false, false, true);
+                    playerEntity.addStatusEffect(badOmen);
+                    Meson.LOG.debug("Applying bad omen of amplifier: " + amplifier);
+                }
             }
 
             return ActionResult.SUCCESS;
