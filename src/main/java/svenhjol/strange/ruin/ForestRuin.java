1dbd1d6367ffb0888fe521d7b806ad3c819ff77a
Coranthes <josa23oreo@gmail.com>
2020-10-07 14:55:14 -0400

    Forest Ruin WIP

--- src/main/java/svenhjol/strange/ruin/ForestRuin.java
+++ src/main/java/svenhjol/strange/ruin/ForestRuin.java
@@ -8,7 +8,7 @@ import java.util.Map;
 public class ForestRuin extends BaseRuin {
     private final Map<String, Integer> ROOMS = new HashMap<>();
     private final Map<String, Integer> CORRIDORS = new HashMap<>();
-  //  private final Map<String, Integer> ENDS = new HashMap<>();
+    private final Map<String, Integer> ENDS = new HashMap<>();
 
     public ForestRuin() {
         super(Strange.MOD_ID, "forest_ruin");
@@ -17,21 +17,21 @@ public class ForestRuin extends BaseRuin {
 
        ROOMS.put("room1", 1);
        ROOMS.put("room2", 1);
-    ///    ROOMS.put("room_corner2", 1);
-    ///    ROOMS.put("room_derp1", 1);
+       ROOMS.put("room3", 1);
+       ROOMS.put("room4", 1);
     ///    ROOMS.put("room_skeletons", 1)
     //   ROOMS.put("room_trap", 1);
     //    ROOMS.put("room_zombies", 1);
 
         CORRIDORS.put("corridor", 1);
-    //   CORRIDORS.put("corridor_shattered", 1);
-    //    CORRIDORS.put("corridor_small", 1);
-    //    CORRIDORS.put("corridor_spawner", 1);
+       CORRIDORS.put("corridor1", 1);
+        CORRIDORS.put("corridor2", 1);
+        CORRIDORS.put("corridor3", 1);
     //    CORRIDORS.put("corridor_tiny", 1);
 
-   //     ENDS.put("end_chest", 1);
-   //     ENDS.put("end_erosion", 1);
-   //     ENDS.put("end_potions", 1);
+        ENDS.put("end1", 1);
+        ENDS.put("end2", 1);
+        ENDS.put("end3", 1);
     //    ENDS.put("end_wall1", 1);
     //    ENDS.put("end_wall2", 1);
     //    ENDS.put("end_wall3", 1);
@@ -39,6 +39,6 @@ public class ForestRuin extends BaseRuin {
 
         registerPool("rooms", ROOMS);
         registerPool("corridors", CORRIDORS);
-    //    registerPool("ends", ENDS);
+        registerPool("ends", ENDS);
     }
 }
