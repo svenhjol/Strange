590a5b7917fb23cc5a7b98491eacb4d41ef205af
Sven Hjól <sven.hjol@protonmail.com>
2020-10-20 00:00:05 +0000

    Start foundation loot

--- src/main/java/svenhjol/strange/base/StrangeLoot.java
+++ src/main/java/svenhjol/strange/base/StrangeLoot.java
@@ -6,8 +6,10 @@ import svenhjol.strange.Strange;
 
 public class StrangeLoot {
     public static Identifier ANCIENT_RUBBLE = new Identifier(Strange.MOD_ID, "gameplay/ancient_rubble");
+    public static Identifier FOUNDATIONS = new Identifier(Strange.MOD_ID, "gameplay/foundations");
 
     public static void init() {
         LootHelper.CUSTOM_LOOT_TABLES.add(ANCIENT_RUBBLE);
+        LootHelper.CUSTOM_LOOT_TABLES.add(FOUNDATIONS);
     }
 }
