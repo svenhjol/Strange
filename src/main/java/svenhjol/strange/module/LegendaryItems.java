1bc76c220b4346a31d7a2c61e04ed69ed2b03a49
Sven Hjól <sven.hjol@protonmail.com>
2020-10-28 02:50:09 +0000

    Slight bias towards items in rubble

--- src/main/java/svenhjol/strange/module/LegendaryItems.java
+++ src/main/java/svenhjol/strange/module/LegendaryItems.java
@@ -70,7 +70,7 @@ public class LegendaryItems extends CharmModule {
                 lootTable = LootTables.SIMPLE_DUNGEON_CHEST;
             } else {
                 Charm.LOG.info("Adding legendary items to ruin loot");
-                lootTable = StrangeLoot.RUIN_GENERAL;
+                lootTable = StrangeLoot.RUIN_RARE;
             }
         } else {
             Charm.LOG.info("Adding legendary items to ancient_rubble loot");
