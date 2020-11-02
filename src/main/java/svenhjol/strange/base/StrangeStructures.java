707811a22a19a98e8892f97157666b6a40d19d1d
Sven Hjól <sven.hjol@protonmail.com>
2020-11-02 02:32:20 +0000

    Using wrong processor chance

--- src/main/java/svenhjol/strange/base/StrangeStructures.java
+++ src/main/java/svenhjol/strange/base/StrangeStructures.java
@@ -10,13 +10,13 @@ import svenhjol.strange.runestones.RunestoneHelper;
 public class StrangeStructures {
     public static void init() {
         DataBlockProcessor.callbacks.put("rubble", processor -> {
-            if (processor.random.nextFloat() < 0.9F)
+            if (processor.withChance(0.9F))
                 processor.state = Excavation.ANCIENT_RUBBLE.getDefaultState();
         });
 
         DataBlockProcessor.callbacks.put("runestone", processor -> {
             if (ModuleHandler.enabled("strange:runestones")
-                && processor.random.nextFloat() < 0.75F) {
+                && processor.withChance(0.7F)) {
                 processor.state = Runestones.RUNESTONE_BLOCKS.get(processor.random.nextInt(RunestoneHelper.NUMBER_OF_RUNES)).getDefaultState();
             } else {
                 processor.state = Blocks.STONE.getDefaultState();
