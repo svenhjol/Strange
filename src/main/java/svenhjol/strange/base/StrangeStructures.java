495c4535a8825d6e2bb4cabfea4e626bbdd1114a
Sven Hjól <sven.hjol@protonmail.com>
2020-11-02 02:39:51 +0000

    Check module, default to gravel

--- src/main/java/svenhjol/strange/base/StrangeStructures.java
+++ src/main/java/svenhjol/strange/base/StrangeStructures.java
@@ -10,13 +10,15 @@ import svenhjol.strange.runestones.RunestoneHelper;
 public class StrangeStructures {
     public static void init() {
         DataBlockProcessor.callbacks.put("rubble", processor -> {
-            if (processor.withChance(0.9F))
+            if (ModuleHandler.enabled("strange:excavation") && processor.withChance(0.9F)) {
                 processor.state = Excavation.ANCIENT_RUBBLE.getDefaultState();
+            } else {
+                processor.state = Blocks.GRAVEL.getDefaultState();
+            }
         });
 
         DataBlockProcessor.callbacks.put("runestone", processor -> {
-            if (ModuleHandler.enabled("strange:runestones")
-                && processor.withChance(0.7F)) {
+            if (ModuleHandler.enabled("strange:runestones") && processor.withChance(0.7F)) {
                 processor.state = Runestones.RUNESTONE_BLOCKS.get(processor.random.nextInt(RunestoneHelper.NUMBER_OF_RUNES)).getDefaultState();
             } else {
                 processor.state = Blocks.STONE.getDefaultState();
