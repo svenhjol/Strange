8da1a92b1e6e709f31a43a0f8db7a1ca120cf7eb
Sven Hjól <sven.hjol@protonmail.com>
2020-11-02 00:37:07 +0000

    Runic tablets config and descriptions

--- src/main/java/svenhjol/strange/module/RunicTablets.java
+++ src/main/java/svenhjol/strange/module/RunicTablets.java
@@ -6,6 +6,7 @@ import net.fabricmc.fabric.api.loot.v1.event.LootTableLoadingCallback;
 import net.minecraft.item.Items;
 import net.minecraft.loot.ConstantLootTableRange;
 import net.minecraft.loot.LootManager;
+import net.minecraft.loot.LootTables;
 import net.minecraft.loot.condition.LootCondition;
 import net.minecraft.loot.entry.ItemEntry;
 import net.minecraft.loot.function.LootFunctionType;
@@ -29,7 +30,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
-@Module(mod = Strange.MOD_ID)
+@Module(mod = Strange.MOD_ID, description = "Craftable tablets that can teleport you to a point of interest or the location of a lodestone.")
 public class RunicTablets extends CharmModule {
     public static final Identifier RUNIC_FRAGMENT_LOOT_ID = new Identifier(Strange.MOD_ID, "runic_fragment_loot");
     public static LootFunctionType RUNIC_FRAGMENT_LOOT_FUNCTION;
@@ -37,17 +38,19 @@ public class RunicTablets extends CharmModule {
     public static RunicTabletItem RUNIC_TABLET;
     public static RunicFragmentItem RUNIC_FRAGMENT;
 
-    @Config(name = "Knowledge fragment locations", description = "Structures that knowledge fragments may describe. The list is weighted with more likely structures at the top.")
-    public static List<String> configLocations = new ArrayList<>(Arrays.asList(
-        "strange:foundation",
-        "strange:ruin",
-        "strange:stone_circle"
-    ));
-
     public static List<Identifier> destinations = new ArrayList<>();
 
-    public static boolean addFragmentsToRuinLoot = true;
+    @Config(name = "Runic fragment destinations", description = "Structures that runic fragments may describe. The list is weighted with more likely structures at the top.")
+    public static List<String> configStructures = new ArrayList<>(Arrays.asList(
+        "strange:foundation",
+        "strange:ruin"
+    ));
+
+    @Config(name = "Loot chance", description = "Chance (out of 1.0) of a runic fragment appearing in ruin loot (or stronghold loot if ruins are disabled).")
     public static double lootChance = 0.75F;
+
+    @Config(name = "Required levels", description = "Experience levels required to create a runic tablet on a writing desk.")
+    public static int requiredXpLevels = 15;
 
     @Override
     public void register() {
@@ -58,22 +61,24 @@ public class RunicTablets extends CharmModule {
 
     @Override
     public void init() {
-        configLocations.forEach(configLocation -> {
-            Identifier locationId = new Identifier(configLocation);
+        // iterate through config structures, check the registry and add confirmed structures to destinations array
+        configStructures.forEach(configStructure -> {
+            Identifier locationId = new Identifier(configStructure);
             Optional<StructureFeature<?>> location = Registry.STRUCTURE_FEATURE.getOrEmpty(locationId);
 
             if (location.isPresent())
                 destinations.add(locationId);
         });
 
+        // listen for loot events so that we can add the fragment to loot tables on demand
         LootTableLoadingCallback.EVENT.register(this::handleLootTables);
     }
 
     private void handleLootTables(ResourceManager resourceManager, LootManager lootManager, Identifier id, FabricLootSupplierBuilder supplier, LootTableLoadingCallback.LootTableSetter setter) {
-        if (!ModuleHandler.enabled("strange:excavation"))
-            return;
+        boolean ruinsEnabled = ModuleHandler.enabled("strange:ruins");
 
-        if (addFragmentsToRuinLoot && id.equals(StrangeLoot.RUIN_RARE)) {
+        if ((ruinsEnabled && id.equals(StrangeLoot.RUIN_RARE))
+            || (!ruinsEnabled && id.equals(LootTables.STRONGHOLD_CROSSING_CHEST))) {
             FabricLootPoolBuilder builder = FabricLootPoolBuilder.builder()
                 .rolls(ConstantLootTableRange.create(1))
                 .with(ItemEntry.builder(Items.AIR)
