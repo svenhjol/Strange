523a1d4630392d23337778f59d0893d10cb37c36
Sven Hjól <sven.hjol@protonmail.com>
2020-11-26 03:12:16 +0000

    Removed cross icon, replaced with help

--- src/main/java/svenhjol/strange/base/StrangeIcons.java
+++ src/main/java/svenhjol/strange/base/StrangeIcons.java
@@ -9,7 +9,7 @@ public class StrangeIcons {
     public final static int ICON_HEIGHT = 9;
     public final static int[] ICON_STAR = {2, 0};
     public final static int[] ICON_TICK = {3, 0};
-    public final static int[] ICON_CROSS = {4, 0};
+    public final static int[] ICON_HELP = {4, 0};
     public final static int[] ICON_SWORD = {5, 0};
     public final static int[] ICON_WHEAT = {6, 0};
     public final static int[] ICON_COMPASS = {7, 0};
