fa9f5c5fbf8aa64279b33e1e6d4bd7b773469661
Coranthes <josa23oreo@gmail.com>
2020-11-27 14:40:05 -0500

    Castle corridor errors be gone

--- src/main/java/svenhjol/strange/ruins/builds/Castle.java
+++ src/main/java/svenhjol/strange/ruins/builds/Castle.java
@@ -14,25 +14,22 @@ public class Castle extends BaseStructure {
     public Castle() {
         super(Strange.MOD_ID, "ruins", "castle");
 
-    //    addStart("start_trap", 1);
         addStart("start_tower", 1);
         addStart("start_tower2", 1);
 
 
         ROOMS.put("room_secret", 1);
-        ROOMS.put("room_smlib", 2);
+        ROOMS.put("room_smlib", 1);
         ROOMS.put("room_tower", 3);
         ROOMS.put("room_corner_upper", 3);
         ROOMS.put("room_corner_lower", 3);
         ROOMS.put("room_wood", 3);
-        ROOMS.put("room_trap", 3);
-        ROOMS.put("room_nature", 3);
+        ROOMS.put("room_trap", 2);
+        ROOMS.put("room_nature", 2);
 
-    //    CORRIDORS.put("corridor_start1", 3);
-    //    CORRIDORS.put("corridor_start2", 2);
-    //    CORRIDORS.put("corridor_start3", 2);
-        CORRIDORS.put("corridor_stairs", 2);
         CORRIDORS.put("corridor_long", 3);
+        CORRIDORS.put("corridor_stairs", 3);
+        CORRIDORS.put("corridor_long_lower", 3);
         CORRIDORS.put("corridor_med", 2);
         CORRIDORS.put("corridor_med_low", 3);
         CORRIDORS.put("corridor_spawner_lower", 3);
@@ -44,8 +41,6 @@ public class Castle extends BaseStructure {
         ENDS.put("end_deco", 2);
         ENDS.put("end_deco_high", 2);
         ENDS.put("end_fire_high", 2);
-    //    ENDS.put("end_wall3", 1);
-    //    ENDS.put("end_wall4", 1);
 
         registerPool("rooms", ROOMS);
         registerPool("corridors", CORRIDORS);
