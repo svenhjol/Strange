0281bf064e6dc52ec9b35675d50e1626ef561f7d
Coranthes <josa23oreo@gmail.com>
2020-11-26 20:55:23 -0500

    All altar stuff

--- src/main/java/svenhjol/strange/runicaltars/RunicAltarsClient.java
+++ src/main/java/svenhjol/strange/runicaltars/RunicAltarsClient.java
@@ -14,6 +14,6 @@ public class RunicAltarsClient extends CharmClientModule {
     @Override
     public void init() {
         ScreenRegistry.register(RunicAltars.SCREEN_HANDLER, RunicAltarScreen::new);
-        BlockRenderLayerMap.INSTANCE.putBlock(RunicAltars.RUNIC_ALTAR, RenderLayer.getTranslucent());
+        BlockRenderLayerMap.INSTANCE.putBlock(RunicAltars.RUNIC_ALTAR, RenderLayer.getCutout());
     }
 }
