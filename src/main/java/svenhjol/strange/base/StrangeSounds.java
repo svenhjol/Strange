0a25b7d4e14ff5fa9a9a5e26203bd7d9adc78eff
Sven Hjól <sven.hjol@protonmail.com>
2020-12-01 00:45:19 +0000

    Sounds not being registered properly

--- src/main/java/svenhjol/strange/base/StrangeSounds.java
+++ src/main/java/svenhjol/strange/base/StrangeSounds.java
@@ -5,14 +5,23 @@ import net.minecraft.util.Identifier;
 import svenhjol.charm.base.handler.RegistryHandler;
 import svenhjol.strange.Strange;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class StrangeSounds {
+    public static Map<Identifier, SoundEvent> REGISTER = new HashMap<>();
+
     public static final SoundEvent RUNESTONE_TRAVEL = createSound("runestone_travel");
     public static final SoundEvent SCREENSHOT = createSound("screenshot");
+
+    public static void init() {
+        REGISTER.forEach(RegistryHandler::sound);
+    }
 
     public static SoundEvent createSound(String name) {
         Identifier id = new Identifier(Strange.MOD_ID, name);
         SoundEvent sound = new SoundEvent(id);
-        RegistryHandler.sound(id, sound);
+        REGISTER.put(id, sound);
         return sound;
     }
 }
