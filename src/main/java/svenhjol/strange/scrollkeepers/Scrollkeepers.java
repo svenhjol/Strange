b4a1bdcfa4ef7999323c561c53b7ea7c499f75b5
Sven Hjól <sven.hjol@protonmail.com>
2020-12-04 02:43:58 +0000

    Half spawn rate of scrollkeeper houses

--- src/main/java/svenhjol/strange/scrollkeepers/Scrollkeepers.java
+++ src/main/java/svenhjol/strange/scrollkeepers/Scrollkeepers.java
@@ -76,11 +76,11 @@ public class Scrollkeepers extends CharmModule {
 
         // register scrollkeeper structures
         StructureSetupCallback.EVENT.register(() -> {
-            addVillageHouse(StructureSetupCallback.VillageType.PLAINS, new Identifier("strange:village/plains/houses/scrollkeeper"), 10);
-            addVillageHouse(StructureSetupCallback.VillageType.SAVANNA, new Identifier("strange:village/savanna/houses/scrollkeeper"), 10);
-            addVillageHouse(StructureSetupCallback.VillageType.SNOWY, new Identifier("strange:village/snowy/houses/scrollkeeper"), 10);
-            addVillageHouse(StructureSetupCallback.VillageType.TAIGA, new Identifier("strange:village/taiga/houses/scrollkeeper"), 10);
-            addVillageHouse(StructureSetupCallback.VillageType.DESERT, new Identifier("strange:village/desert/houses/scrollkeeper"), 10);
+            addVillageHouse(StructureSetupCallback.VillageType.PLAINS, new Identifier("strange:village/plains/houses/scrollkeeper"), 5);
+            addVillageHouse(StructureSetupCallback.VillageType.SAVANNA, new Identifier("strange:village/savanna/houses/scrollkeeper"), 5);
+            addVillageHouse(StructureSetupCallback.VillageType.SNOWY, new Identifier("strange:village/snowy/houses/scrollkeeper"), 5);
+            addVillageHouse(StructureSetupCallback.VillageType.TAIGA, new Identifier("strange:village/taiga/houses/scrollkeeper"), 5);
+            addVillageHouse(StructureSetupCallback.VillageType.DESERT, new Identifier("strange:village/desert/houses/scrollkeeper"), 5);
 
         });
 
