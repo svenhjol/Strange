ba1a1f98a07c3ec3bccb23e0dff016ab61a5862a
Sven Hjól <sven.hjol@protonmail.com>
2020-12-05 22:58:43 +0000

    Add village scrollkeeper loot table

--- src/main/java/svenhjol/strange/base/StrangeLoot.java
+++ src/main/java/svenhjol/strange/base/StrangeLoot.java
@@ -1,12 +1,15 @@
 package svenhjol.strange.base;
 
 import net.minecraft.util.Identifier;
+import svenhjol.charm.base.helper.LootHelper;
 import svenhjol.strange.Strange;
 
 public class StrangeLoot {
     public static Identifier ANCIENT_RUBBLE = new Identifier(Strange.MOD_ID, "gameplay/ancient_rubble");
     public static Identifier RUIN_RARE = new Identifier(Strange.MOD_ID, "gameplay/ruin_rare");
+    public static Identifier VILLAGE_SCROLLKEEPER = new Identifier(Strange.MOD_ID, "gameplay/village_scrollkeeper");
 
     public static void init() {
+        LootHelper.CUSTOM_LOOT_TABLES.add(VILLAGE_SCROLLKEEPER);
     }
 }
