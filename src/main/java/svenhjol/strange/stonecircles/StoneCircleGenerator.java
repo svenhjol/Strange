40013bf9f41bba0dc1d73316ac6f625e6c8dbf6e
Sven Hjól <sven.hjol@protonmail.com>
2021-03-27 13:23:04 +0000

    Signal fire spawning, rune chance tweaks

--- src/main/java/svenhjol/strange/stonecircles/StoneCircleGenerator.java
+++ src/main/java/svenhjol/strange/stonecircles/StoneCircleGenerator.java
@@ -2,6 +2,8 @@ package svenhjol.strange.stonecircles;
 
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
+import net.minecraft.block.CampfireBlock;
+import net.minecraft.block.PillarBlock;
 import net.minecraft.block.entity.LootableContainerBlockEntity;
 import net.minecraft.loot.LootTables;
 import net.minecraft.nbt.CompoundTag;
@@ -11,6 +13,7 @@ import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockBox;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.ChunkPos;
+import net.minecraft.util.math.Direction;
 import net.minecraft.world.StructureWorldAccess;
 import net.minecraft.world.gen.StructureAccessor;
 import net.minecraft.world.gen.chunk.ChunkGenerator;
@@ -24,8 +27,6 @@ import svenhjol.strange.runestones.Runestones;
 import java.util.*;
 
 public class StoneCircleGenerator extends StructurePieceWithDimensions {
-    public static float chestChance = 0.25F; // the chance that a loot chest will be generated in the middle of the circle
-    public static float runeChance = 0.3F; // the chance that a single stone column will have a rune on top
     public static int maxCheckSurface = 5;
     public static int minCheckSurface = -15;
     public static int maxRadius = 13;
@@ -45,8 +46,7 @@ public class StoneCircleGenerator extends StructurePieceWithDimensions {
     @Override
     public boolean generate(StructureWorldAccess world, StructureAccessor structureAccessor, ChunkGenerator gen, Random random, BlockBox boundingBox, ChunkPos chunkPos, BlockPos blockPos) {
         int radius = random.nextInt(maxRadius - minRadius) + minRadius;
-
-        Identifier lootTable = LootTables.PILLAGER_OUTPOST_CHEST;
+        Identifier lootTable = LootTables.PILLAGER_OUTPOST_CHEST; // TODO: should be stone circle specific loot table
 
         List<BlockState> blocks = new ArrayList<>(Arrays.asList(
             Blocks.STONE.getDefaultState(),
@@ -68,6 +68,7 @@ public class StoneCircleGenerator extends StructurePieceWithDimensions {
             return false;
         }
 
+        int numberOfRunesGenerated = 0;
         for (int i = 0; i < 360; i += 45) {
             double x = radius * Math.cos(i * Math.PI / 180);
             double z = radius * Math.sin(i * Math.PI / 180);
@@ -90,7 +91,6 @@ public class StoneCircleGenerator extends StructurePieceWithDimensions {
 
                 for (int y = 1; y < height; y++) {
                     BlockState state = blocks.get(random.nextInt(blocks.size()));
-
                     boolean isTop = y == height - 1;
 
                     if (isTop) {
@@ -98,7 +98,8 @@ public class StoneCircleGenerator extends StructurePieceWithDimensions {
                             // generate a spawn destination rune
                             state = Runestones.RUNESTONE_BLOCKS.get(Runestones.SPAWN_RUNE).getDefaultState();
                             generatedSpawnRune = true;
-                        } else if (random.nextFloat() < runeChance) {
+                        } else if (numberOfRunesGenerated < 3 && random.nextFloat() < 0.4F - (numberOfRunesGenerated * 0.1F)) {
+
                             // Try and generate a rune. Replace the state with the runestone if successful
                             for (int tries = 0; tries < runeTries; tries++) {
                                 List<Integer> keys = new ArrayList<>(availableRunes.keySet());
@@ -112,6 +113,7 @@ public class StoneCircleGenerator extends StructurePieceWithDimensions {
 
                                 availableRunes.remove(rune);
                                 state = Runestones.RUNESTONE_BLOCKS.get(rune).getDefaultState();
+                                ++numberOfRunesGenerated;
                                 break;
                             }
                         }
@@ -128,9 +130,8 @@ public class StoneCircleGenerator extends StructurePieceWithDimensions {
             }
         }
 
-        // generate chest at center
-        if (random.nextFloat() < chestChance) {
-            boolean useVariantChests = ModuleHandler.enabled("charm:variant_chests");
+        // generate chest or campfire at center
+        if (random.nextFloat() < 0.75F) {
             for (int s = maxCheckSurface; s > minCheckSurface; s--) {
                 BlockPos checkPos = blockPos.add(0, s, 0);
                 BlockPos checkUpPos = checkPos.up();
@@ -138,16 +139,28 @@ public class StoneCircleGenerator extends StructurePieceWithDimensions {
                 BlockState checkUpState = world.getBlockState(checkUpPos);
 
                 if (checkState.isOpaque() && !checkUpState.isOpaque() && !checkUpState.getMaterial().isLiquid() && lootTable != null) {
-                    BlockState chest;
-                    if (useVariantChests) {
-                        IVariantMaterial material = DecorationHelper.getRandomVariantMaterial(random);
-                        chest = VariantChests.NORMAL_CHEST_BLOCKS.get(material).getDefaultState();
-                    } else {
-                        chest = Blocks.CHEST.getDefaultState();
-                    }
 
-                    world.setBlockState(checkUpPos, chest, 2);
-                    LootableContainerBlockEntity.setLootTable(world, random, checkUpPos, lootTable);
+                    boolean generateChest = random.nextBoolean();
+
+                    if (generateChest) {
+                        BlockState chest;
+                        if (ModuleHandler.enabled("charm:variant_chests")) {
+                            IVariantMaterial material = DecorationHelper.getRandomVariantMaterial(random);
+                            chest = VariantChests.NORMAL_CHEST_BLOCKS.get(material).getDefaultState();
+                        } else {
+                            chest = Blocks.CHEST.getDefaultState();
+                        }
+                        world.setBlockState(checkUpPos, chest, 2);
+                        LootableContainerBlockEntity.setLootTable(world, random, checkUpPos, lootTable);
+                    } else {
+                        BlockState hay = Blocks.HAY_BLOCK.getDefaultState().with(PillarBlock.AXIS, Direction.Axis.Y);
+                        BlockState fire = Blocks.CAMPFIRE.getDefaultState()
+                            .with(CampfireBlock.LIT, true)
+                            .with(CampfireBlock.SIGNAL_FIRE, true);
+
+                        world.setBlockState(checkPos, hay, 2);
+                        world.setBlockState(checkUpPos, fire, 2);
+                    }
                     break;
                 }
             }
