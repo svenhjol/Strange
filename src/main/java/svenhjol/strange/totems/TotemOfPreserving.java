8e5960ec9661179a3174e568e86be1fafd1395b2
Sven Hjól <sven.hjol@protonmail.com>
2021-04-02 00:10:05 +0000

    Totem of Preserving is now always in "grave mode"

--- src/main/java/svenhjol/strange/totems/TotemOfPreserving.java
+++ src/main/java/svenhjol/strange/totems/TotemOfPreserving.java
@@ -1,38 +1,25 @@
 package svenhjol.strange.totems;
 
 import com.google.common.collect.ImmutableList;
-import net.fabricmc.fabric.api.loot.v1.FabricLootPoolBuilder;
-import net.fabricmc.fabric.api.loot.v1.FabricLootSupplierBuilder;
-import net.fabricmc.fabric.api.loot.v1.event.LootTableLoadingCallback;
 import net.minecraft.advancement.criterion.Criteria;
 import net.minecraft.entity.ItemEntity;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.damage.DamageSource;
-import net.minecraft.entity.mob.IllusionerEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.item.ItemStack;
-import net.minecraft.loot.LootManager;
-import net.minecraft.loot.LootTables;
-import net.minecraft.loot.entry.ItemEntry;
-import net.minecraft.loot.provider.number.UniformLootNumberProvider;
 import net.minecraft.nbt.NbtCompound;
-import net.minecraft.resource.ResourceManager;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.text.TranslatableText;
 import net.minecraft.util.ActionResult;
-import net.minecraft.util.Identifier;
 import net.minecraft.util.collection.DefaultedList;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
 import svenhjol.charm.Charm;
 import svenhjol.charm.base.CharmModule;
 import svenhjol.charm.base.helper.DimensionHelper;
 import svenhjol.charm.base.helper.ItemHelper;
 import svenhjol.charm.base.iface.Config;
 import svenhjol.charm.base.iface.Module;
-import svenhjol.charm.event.EntityDropsCallback;
 import svenhjol.charm.event.EntityDropsXpCallback;
 import svenhjol.charm.event.PlayerDropInventoryCallback;
 import svenhjol.strange.Strange;
@@ -42,14 +29,9 @@ import svenhjol.strange.traveljournals.TravelJournals;
 
 import java.util.*;
 
-import static net.minecraft.item.Items.TOTEM_OF_UNDYING;
-
 @Module(mod = Strange.MOD_ID, description = "With a Totem of Preserving in your inventory, your items will be held in the totem when you die.")
 public class TotemOfPreserving extends CharmModule {
     public static TotemOfPreservingItem TOTEM_OF_PRESERVING;
-
-    @Config(name = "Grave Mode", description = "If true, your items will always drop as a totem even if you don't have one in your inventory.")
-    public static boolean graveMode = true;
 
     @Config(name = "Preserve XP", description = "If true, the totem will preserve the player's experience and restore when broken.")
     public static boolean preserveXp = false;
@@ -64,8 +46,6 @@ public class TotemOfPreserving extends CharmModule {
         ItemHelper.ITEM_LIFETIME.put(TOTEM_OF_PRESERVING, Integer.MAX_VALUE); // probably stupid
         PlayerDropInventoryCallback.EVENT.register(this::tryInterceptDropInventory);
         EntityDropsXpCallback.BEFORE.register(this::tryInterceptDropXp);
-        LootTableLoadingCallback.EVENT.register(this::handleLootTables);
-        EntityDropsCallback.AFTER.register(this::tryDropTotemFromIllusioner);
     }
 
     public ActionResult tryInterceptDropXp(LivingEntity entity) {
@@ -88,26 +68,6 @@ public class TotemOfPreserving extends CharmModule {
 
         combined.forEach(list
             -> list.stream().filter(Objects::nonNull).filter(stack -> !stack.isEmpty()).forEach(holdable::add));
-
-        // check all inventories for totem
-        if (!graveMode) {
-            boolean found = false;
-            int foundAtIndex = 0;
-
-            for (int i = 0; i < holdable.size(); i++) {
-                ItemStack stack = holdable.get(i);
-                if (stack.getItem() == TOTEM_OF_PRESERVING && TotemOfPreservingItem.getItems(stack).isEmpty()) {
-                    foundAtIndex = i;
-                    found = true;
-                }
-            }
-
-            if (found) {
-                holdable.remove(foundAtIndex);
-            } else {
-                return ActionResult.FAIL;
-            }
-        }
 
         List<ItemStack> totemsToSpawn = new ArrayList<>();
 
@@ -182,31 +142,5 @@ public class TotemOfPreserving extends CharmModule {
         }
 
         return ActionResult.SUCCESS;
-    }
-
-    private void handleLootTables(ResourceManager resourceManager, LootManager lootManager, Identifier id, FabricLootSupplierBuilder supplier, LootTableLoadingCallback.LootTableSetter setter) {
-        if (graveMode)
-            return;
-
-        if (id.equals(LootTables.PILLAGER_OUTPOST_CHEST) || id.equals(LootTables.WOODLAND_MANSION_CHEST)) {
-            FabricLootPoolBuilder builder = FabricLootPoolBuilder.builder()
-                .rolls(UniformLootNumberProvider.create(0.0F, 1.0F))
-                .with(ItemEntry.builder(TOTEM_OF_PRESERVING));
-
-            supplier.pool(builder);
-        }
-    }
-
-    public ActionResult tryDropTotemFromIllusioner(LivingEntity entity, DamageSource source, int lootingLevel) {
-        if (!entity.world.isClient
-            && entity instanceof IllusionerEntity
-        ) {
-            World world = entity.getEntityWorld();
-            BlockPos pos = entity.getBlockPos();
-
-            world.spawnEntity(new ItemEntity(world, pos.getX(), pos.getY(), pos.getZ(), new ItemStack(TOTEM_OF_UNDYING)));
-        }
-
-        return ActionResult.PASS;
     }
 }
