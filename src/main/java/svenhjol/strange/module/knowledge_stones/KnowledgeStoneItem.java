fda2c171ffd7a4ac80e462e3e106f10ea6fbcfda
Sven Hjól <sven.hjol@protonmail.com>
2021-10-28 00:35:04 +0000

    Knowledge stones now use types as enum

--- src/main/java/svenhjol/strange/module/knowledge_stones/KnowledgeStoneItem.java
+++ src/main/java/svenhjol/strange/module/knowledge_stones/KnowledgeStoneItem.java
@@ -10,8 +10,9 @@ import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Rarity;
 import net.minecraft.world.level.Level;
+import svenhjol.charm.enums.ICharmEnum;
+import svenhjol.charm.helper.PlayerHelper;
 import svenhjol.charm.helper.StringHelper;
 import svenhjol.charm.item.CharmItem;
 import svenhjol.charm.loader.CharmModule;
@@ -22,89 +23,85 @@ import svenhjol.strange.module.knowledge.KnowledgeBranch;
 import svenhjol.strange.module.knowledge.KnowledgeData;
 
 import java.util.*;
-import java.util.function.Consumer;
+import java.util.function.Function;
 
 public class KnowledgeStoneItem extends CharmItem {
-    public static final String RARITY_TAG = "Rarity";
+    private final Type type;
 
-    public KnowledgeStoneItem(CharmModule module) {
-        super(module, "knowledge_stone", new FabricItemSettings()
+    public KnowledgeStoneItem(CharmModule module, Type type) {
+        super(module, type.getSerializedName() + "_knowledge_stone", new FabricItemSettings()
             .tab(CreativeModeTab.TAB_MISC)
             .stacksTo(64));
-    }
 
-    @Override
-    public boolean isFoil(ItemStack stone) {
-        Rarity rarity = getKnowledgeRarity(stone);
-        return rarity == Rarity.RARE || rarity == Rarity.EPIC;
+        this.type = type;
     }
 
     @Override
     public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand) {
-        ItemStack stack = player.getItemInHand(hand);
+        ItemStack held = player.getItemInHand(hand);
+        KnowledgeStoneItem stone = (KnowledgeStoneItem)held.getItem();
         JournalData journal = Journals.getJournalData(player).orElseThrow();
         KnowledgeData knowledge = Knowledge.getSavedData().orElseThrow();
-        Rarity rarity = getKnowledgeRarity(stack);
 
         if (!level.isClientSide) {
             ServerPlayer serverPlayer = (ServerPlayer) player;
             MinecraftServer server = serverPlayer.getServer();
             if (server == null) {
-                return InteractionResultHolder.fail(stack);
+                return InteractionResultHolder.fail(held);
             }
 
-            boolean learnedThing = switch (rarity) {
-                case COMMON -> tryLearnFromBranch(journal.getLearnedBiomes(), knowledge.biomes, b -> {
+            boolean learnedThing = switch (stone.type) {
+                case BIOME -> tryLearnFromBranch(journal.getLearnedBiomes(), knowledge.biomes, b -> {
                     journal.learnBiome(b);
                     clientMessage(serverPlayer, "biome", b.getPath());
+                    return true;
                 });
 
-                case UNCOMMON -> tryLearnFromBranch(journal.getLearnedStructures(), knowledge.structures, s -> {
+                case STRUCTURE -> tryLearnFromBranch(journal.getLearnedStructures(), knowledge.structures, s -> {
                     journal.learnStructure(s);
                     clientMessage(serverPlayer, "structure", s.getPath());
+                    return true;
                 });
 
-                case RARE -> tryLearnFromBranch(journal.getLearnedPlayers(), knowledge.players, p -> {
+                case PLAYER -> tryLearnFromBranch(journal.getLearnedPlayers(), knowledge.players, p -> {
                     journal.learnPlayer(p);
-                    String playerName;
-
-                    // TODO: move all this to PlayerHelper
-                    Optional<ServerPlayer> learnedPlayer = Optional.ofNullable(server.getPlayerList().getPlayer(p));
-                    if (learnedPlayer.isPresent()) {
-                        playerName = learnedPlayer.get().getName().getString();
+                    Optional<String> optName = PlayerHelper.getPlayerName(server, p);
+                    if (optName.isPresent()) {
+                        clientMessage(serverPlayer, "player", optName.get());
+                        return true;
                     } else {
-                        playerName = p.toString();
+                        return false;
                     }
-
-                    clientMessage(serverPlayer, "player", playerName);
                 });
 
-                case EPIC -> tryLearnFromBranch(journal.getLearnedDimensions(), knowledge.dimensions, d -> {
+                case DIMENSION -> tryLearnFromBranch(journal.getLearnedDimensions(), knowledge.dimensions, d -> {
                     journal.learnDimension(d);
                     clientMessage(serverPlayer, "dimension", d.getPath());
+                    return true;
                 });
             };
 
             if (learnedThing) {
                 // TODO: rune glyph effect and sound effect
             } else {
-                player.giveExperienceLevels((rarity.ordinal() + 1) * 2);
+                player.giveExperienceLevels(10);
             }
 
-            stack.shrink(1);
+            if (!player.getAbilities().instabuild) {
+                held.shrink(1);
+            }
         }
 
-        return InteractionResultHolder.success(stack);
+        return InteractionResultHolder.consume(held);
     }
 
-    private <T> boolean tryLearnFromBranch(List<T> playerKnowledge, KnowledgeBranch<?, T> branch, Consumer<T> onLearn) {
+    private <T> boolean tryLearnFromBranch(List<T> playerKnowledge, KnowledgeBranch<?, T> branch, Function<T, Boolean> onLearn) {
         if (playerKnowledge.size() < branch.size()) {
             List<T> list = new ArrayList<>(branch.values());
             Collections.shuffle(list, new Random());
             for (T item : list) {
                 if (!playerKnowledge.contains(item)) {
-                    onLearn.accept(item);
-                    return true;
+                    return onLearn.apply(item);
                 }
             }
         }
@@ -116,28 +113,10 @@ public class KnowledgeStoneItem extends CharmItem {
         player.displayClientMessage(component, true);
     }
 
-    public static void setKnowledgeRarity(ItemStack stone, Rarity rarity) {
-        stone.getOrCreateTag().putString(RARITY_TAG, rarity.name());
-    }
-
-    public static Rarity getKnowledgeRarity(ItemStack stone) {
-        Rarity rarity;
-        String rarityNbt = stone.getOrCreateTag().getString(RARITY_TAG);
-
-        try {
-            rarity = Rarity.valueOf(rarityNbt);
-        } catch (Exception e) {
-            rarity = Rarity.COMMON;
-        }
-
-        return rarity;
-    }
-
-    public static ItemStack createWithRarity(Rarity rarity) {
-        ItemStack stone = new ItemStack(KnowledgeStones.KNOWLEDGE_STONE);
-        setKnowledgeRarity(stone, rarity);
-        Component component = new TranslatableComponent("item.strange.knowledge_stone_with_rarity", StringHelper.capitalize(rarity.name().toLowerCase(Locale.ROOT)));
-        stone.setHoverName(component);
-        return stone;
+    public enum Type implements ICharmEnum {
+        BIOME,
+        STRUCTURE,
+        PLAYER,
+        DIMENSION;
     }
 }
--- src/main/java/svenhjol/strange/module/knowledge_stones/KnowledgeStones.java
+++ src/main/java/svenhjol/strange/module/knowledge_stones/KnowledgeStones.java
@@ -4,12 +4,17 @@ import svenhjol.charm.annotation.CommonModule;
 import svenhjol.charm.loader.CharmModule;
 import svenhjol.strange.Strange;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @CommonModule(mod = Strange.MOD_ID)
 public class KnowledgeStones extends CharmModule {
-    public static KnowledgeStoneItem KNOWLEDGE_STONE;
+    public static Map<KnowledgeStoneItem.Type, KnowledgeStoneItem> KNOWLEDGE_STONES = new HashMap<>();
 
     @Override
     public void register() {
-        KNOWLEDGE_STONE = new KnowledgeStoneItem(this);
+        for (KnowledgeStoneItem.Type type : KnowledgeStoneItem.Type.values()) {
+            KNOWLEDGE_STONES.put(type, new KnowledgeStoneItem(this, type));
+        }
     }
 }
