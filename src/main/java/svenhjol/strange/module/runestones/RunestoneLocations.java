7e206e15484b0f2e055d142211530fa8285c58d1
Sven Hjól <sven.hjol@protonmail.com>
2021-11-13 02:09:43 +0000

    Redundant

--- src/main/java/svenhjol/strange/module/runestones/RunestoneLocations.java
+++ src/main/java/svenhjol/strange/module/runestones/RunestoneLocations.java
@@ -13,9 +13,6 @@ public class RunestoneLocations {
     public static final ResourceLocation SPAWN = new ResourceLocation(Strange.MOD_ID, "spawn_point");
 
     public static void init() {
-        // assemble map of dimension -> destinations first
-        Map<ResourceLocation, List<ResourceLocation>> dimensionDestinations = new HashMap<>();
-
         for (int i = 0; i < Runestones.configDestinations.size(); i++) {
             List<String> split = StringHelper.splitConfigEntry(Runestones.configDestinations.get(i));
             if (split.size() != 2) continue;
