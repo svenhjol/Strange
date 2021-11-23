3e61c639b3b71410f922fb3fa7dc4ffc3b1d653b
Sven Hjól <sven.hjol@protonmail.com>
2021-11-23 21:40:47 +0000

    Removed unused particle

--- src/main/java/svenhjol/strange/init/StrangeClientParticles.java
+++ src/main/java/svenhjol/strange/init/StrangeClientParticles.java
@@ -6,7 +6,6 @@ import svenhjol.strange.client.StrangeParticle;
 public class StrangeClientParticles {
     public static void init() {
         ParticleFactoryRegistry.getInstance().register(StrangeParticles.APPLY_PARTICLE, StrangeParticle.ApplyFactory::new);
-        ParticleFactoryRegistry.getInstance().register(StrangeParticles.AXIS_PARTICLE, StrangeParticle.AxisFactory::new);
         ParticleFactoryRegistry.getInstance().register(StrangeParticles.ORE_GLOW_PARTICLE, StrangeParticle.OreGlowFactory::new);
     }
 }
--- src/main/java/svenhjol/strange/init/StrangeParticles.java
+++ src/main/java/svenhjol/strange/init/StrangeParticles.java
@@ -7,12 +7,10 @@ import svenhjol.strange.Strange;
 
 public class StrangeParticles {
     public static SimpleParticleType APPLY_PARTICLE;
-    public static SimpleParticleType AXIS_PARTICLE;
     public static SimpleParticleType ORE_GLOW_PARTICLE;
 
     public static void init() {
         APPLY_PARTICLE = RegistryHelper.defaultParticleType(new ResourceLocation(Strange.MOD_ID, "apply"));
-        AXIS_PARTICLE = RegistryHelper.defaultParticleType(new ResourceLocation(Strange.MOD_ID, "axis"));
         ORE_GLOW_PARTICLE = RegistryHelper.defaultParticleType(new ResourceLocation(Strange.MOD_ID, "ore_glow"));
     }
 }
