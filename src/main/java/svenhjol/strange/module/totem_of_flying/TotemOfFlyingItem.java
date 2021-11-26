a36a61001c202390dc4ce4febc78e24ce73b1bb8
Sven Hjól <sven.hjol@protonmail.com>
2021-11-26 17:48:08 +0000

    Add TotemOfFlying

---
+++ src/main/java/svenhjol/strange/module/totem_of_flying/TotemOfFlyingItem.java
@@ -0,0 +1,21 @@
+package svenhjol.strange.module.totem_of_flying;
+
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Rarity;
+import svenhjol.charm.item.CharmItem;
+import svenhjol.charm.loader.CharmModule;
+
+public class TotemOfFlyingItem extends CharmItem {
+    public TotemOfFlyingItem(CharmModule module) {
+        super(module, "totem_of_flying", new Properties()
+            .tab(CreativeModeTab.TAB_MISC)
+            .rarity(Rarity.RARE)
+            .stacksTo(1));
+    }
+
+    @Override
+    public boolean isEnchantable(ItemStack stack) {
+        return false;
+    }
+}
