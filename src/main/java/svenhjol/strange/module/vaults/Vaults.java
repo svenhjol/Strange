48a9f84ed44ca21fe030dddbf7beb635352e3358
Sven Hjól <sven.hjol@protonmail.com>
2021-12-14 20:17:15 +0000

    Add the Vaults structure

--- src/main/java/svenhjol/strange/module/vaults/Vaults.java
+++ src/main/java/svenhjol/strange/module/vaults/Vaults.java
@@ -1,8 +1,34 @@
 package svenhjol.strange.module.vaults;
 
+import com.google.common.collect.ImmutableList;
+import net.fabricmc.fabric.api.event.Event;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerWorldEvents;
+import net.fabricmc.fabric.api.structure.v1.FabricStructureBuilder;
+import net.minecraft.data.worldgen.PlainVillagePools;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.biome.Biome;
+import net.minecraft.world.level.levelgen.GenerationStep;
+import net.minecraft.world.level.levelgen.feature.ConfiguredStructureFeature;
+import net.minecraft.world.level.levelgen.feature.StructureFeature;
+import net.minecraft.world.level.levelgen.feature.configurations.JigsawConfiguration;
+import net.minecraft.world.level.levelgen.structure.templatesystem.StructureProcessorList;
 import svenhjol.charm.annotation.CommonModule;
+import svenhjol.charm.helper.BiomeHelper;
+import svenhjol.charm.helper.DimensionHelper;
+import svenhjol.charm.helper.WorldHelper;
 import svenhjol.charm.loader.CharmModule;
+import svenhjol.charm.registry.CommonRegistry;
 import svenhjol.strange.Strange;
+import svenhjol.strange.module.dimensions.Dimensions;
+import svenhjol.strange.module.dimensions.mirror.MirrorDimension;
+import svenhjol.strange.module.structures.Processors;
+import svenhjol.strange.module.structures.processor.AnvilDamageProcessor;
+import svenhjol.strange.module.structures.processor.StoneBricksDecayProcessor;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Loot ref
@@ -16,4 +42,69 @@ import svenhjol.strange.Strange;
  */
 @CommonModule(mod = Strange.MOD_ID)
 public class Vaults extends CharmModule {
+    public static final ResourceLocation VAULTS_ID = new ResourceLocation(Strange.MOD_ID, "vaults");
+    public static final ResourceLocation WORLD_LOAD_PHASE = new ResourceLocation(Strange.MOD_ID, "world_load_phase");
+
+    public static StructureFeature<JigsawConfiguration> VAULTS_FEATURE;
+    public static ConfiguredStructureFeature<JigsawConfiguration, ? extends StructureFeature<JigsawConfiguration>> CONFIGURED_FEATURE;
+    public static StructureProcessorList VAULTS;
+
+    public static int vaultsSize = 7;
+
+    public static boolean onlyInMirrorDimension = true;
+
+    public static List<String> biomeCategories = Arrays.asList(
+        "plains", "desert", "mountains", "savanna", "forest", "icy", "mesa"
+    );
+
+    @Override
+    public void register() {
+        int size = Math.max(0, Math.min(10, vaultsSize));
+
+        VAULTS_FEATURE = new VaultsFeature(
+            JigsawConfiguration.CODEC,
+            new ResourceLocation(Strange.MOD_ID, "vaults/starts"),
+            size,
+            8,
+            16
+        );
+
+        CONFIGURED_FEATURE = VAULTS_FEATURE.configured(new JigsawConfiguration(() -> PlainVillagePools.START, 0));
+
+        FabricStructureBuilder.create(VAULTS_ID, VAULTS_FEATURE)
+            .step(GenerationStep.Decoration.UNDERGROUND_STRUCTURES)
+            .defaultConfig(32, 18,334020111)
+            .register();
+
+        CommonRegistry.configuredStructureFeature(new ResourceLocation(Strange.MOD_ID, "vaults"), CONFIGURED_FEATURE);
+
+        VAULTS = CommonRegistry.processorList(new ResourceLocation(Strange.MOD_ID, "vaults"), ImmutableList.of(
+            Processors.IGNORE,
+            StoneBricksDecayProcessor.INSTANCE,
+            AnvilDamageProcessor.INSTANCE
+        ));
+    }
+
+    @Override
+    public void runWhenEnabled() {
+        ServerWorldEvents.LOAD.addPhaseOrdering(Event.DEFAULT_PHASE, WORLD_LOAD_PHASE);
+        ServerWorldEvents.LOAD.register(WORLD_LOAD_PHASE, this::handleWorldLoad);
+
+        for (String configCategory : biomeCategories) {
+            Biome.BiomeCategory category = BiomeHelper.getBiomeCategoryByName(configCategory);
+            if (category != null) {
+                BiomeHelper.addStructureToBiomeCategory(CONFIGURED_FEATURE, category);
+            }
+        }
+    }
+
+    private void handleWorldLoad(MinecraftServer server, ServerLevel level) {
+        if (Strange.LOADER.isEnabled(Dimensions.class)
+            && Dimensions.loadMirrorDimension
+            && onlyInMirrorDimension
+            && !DimensionHelper.isDimension(level, MirrorDimension.ID)
+        ) {
+            WorldHelper.removeStructures(level, List.of(VAULTS_FEATURE));
+        }
+    }
 }
---
+++ src/main/java/svenhjol/strange/module/vaults/VaultsFeature.java
@@ -0,0 +1,21 @@
+package svenhjol.strange.module.vaults;
+
+import com.mojang.serialization.Codec;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.levelgen.LegacyRandomSource;
+import net.minecraft.world.level.levelgen.WorldgenRandom;
+import net.minecraft.world.level.levelgen.feature.configurations.JigsawConfiguration;
+import net.minecraft.world.level.levelgen.structure.pieces.PieceGeneratorSupplier;
+import svenhjol.strange.module.structures.RandomHeightJigsawFeature;
+
+public class VaultsFeature extends RandomHeightJigsawFeature {
+    public VaultsFeature(Codec<JigsawConfiguration> codec, ResourceLocation starts, int size, int startY, int variation) {
+        super(codec, starts, size, startY, variation, false, VaultsFeature::checkLocation);
+    }
+
+    private static boolean checkLocation(PieceGeneratorSupplier.Context<JigsawConfiguration> context) {
+        WorldgenRandom worldgenRandom = new WorldgenRandom(new LegacyRandomSource(0L));
+        worldgenRandom.setLargeFeatureSeed(context.seed(), context.chunkPos().x, context.chunkPos().z);
+        return worldgenRandom.nextInt(5) >= 2;
+    }
+}
