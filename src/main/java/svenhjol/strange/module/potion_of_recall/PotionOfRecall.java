5cdd9bc300b8b19229252f3794bf988438b7b4fe
Sven Hjól <sven.hjol@protonmail.com>
2021-12-15 19:37:57 +0000

    PotionOfRecall now gets added to loot

--- src/main/java/svenhjol/strange/module/potion_of_recall/PotionOfRecall.java
+++ src/main/java/svenhjol/strange/module/potion_of_recall/PotionOfRecall.java
@@ -1,17 +1,59 @@
 package svenhjol.strange.module.potion_of_recall;
 
+import net.fabricmc.fabric.api.loot.v1.FabricLootPoolBuilder;
+import net.fabricmc.fabric.api.loot.v1.FabricLootSupplierBuilder;
+import net.fabricmc.fabric.api.loot.v1.event.LootTableLoadingCallback;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.packs.resources.ResourceManager;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.storage.loot.BuiltInLootTables;
+import net.minecraft.world.level.storage.loot.LootTables;
+import net.minecraft.world.level.storage.loot.entries.LootItem;
+import net.minecraft.world.level.storage.loot.functions.LootItemFunctionType;
+import net.minecraft.world.level.storage.loot.predicates.LootItemCondition;
+import net.minecraft.world.level.storage.loot.providers.number.ConstantValue;
 import svenhjol.charm.annotation.CommonModule;
 import svenhjol.charm.loader.CharmModule;
+import svenhjol.charm.registry.CommonRegistry;
 import svenhjol.strange.Strange;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @CommonModule(mod = Strange.MOD_ID)
 public class PotionOfRecall extends CharmModule {
     public static RecallPotion RECALL_POTION;
     public static RecallEffect RECALL_EFFECT;
+    public static ResourceLocation LOOT_ID = new ResourceLocation(Strange.MOD_ID, "potion_of_recall_loot");
+    public static LootItemFunctionType LOOT_FUNCTION;
+
+    private final List<ResourceLocation> VALID_LOOT_TABLES = new ArrayList<>();
+
+    public static boolean onlyOutsideOverworld = true;
 
     @Override
     public void register() {
         RECALL_EFFECT = new RecallEffect(this);
         RECALL_POTION = new RecallPotion(this);
+        LOOT_FUNCTION = CommonRegistry.lootFunctionType(LOOT_ID, new LootItemFunctionType(new PotionOfRecallLootFunction.Serializer()));
+    }
+
+    @Override
+    public void runWhenEnabled() {
+        VALID_LOOT_TABLES.add(BuiltInLootTables.SIMPLE_DUNGEON);
+
+        LootTableLoadingCallback.EVENT.register(this::handleLootTables);
+    }
+
+    private void handleLootTables(ResourceManager resourceManager, LootTables lootTables, ResourceLocation id, FabricLootSupplierBuilder supplier, LootTableLoadingCallback.LootTableSetter lootTableSetter) {
+        if (VALID_LOOT_TABLES.contains(id)) {
+            FabricLootPoolBuilder builder = FabricLootPoolBuilder.builder()
+                .rolls(ConstantValue.exactly(1))
+                .with(LootItem.lootTableItem(Items.AIR)
+                    .setWeight(1)
+                    .apply(() -> new PotionOfRecallLootFunction(new LootItemCondition[0])));
+
+            supplier.withPool(builder);
+        }
     }
 }
---
+++ src/main/java/svenhjol/strange/module/potion_of_recall/PotionOfRecallLootFunction.java
@@ -0,0 +1,51 @@
+package svenhjol.strange.module.potion_of_recall;
+
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonObject;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.storage.loot.LootContext;
+import net.minecraft.world.level.storage.loot.functions.LootItemConditionalFunction;
+import net.minecraft.world.level.storage.loot.functions.LootItemFunctionType;
+import net.minecraft.world.level.storage.loot.predicates.LootItemCondition;
+import svenhjol.charm.helper.DimensionHelper;
+import svenhjol.charm.helper.PotionHelper;
+import svenhjol.strange.Strange;
+
+import java.util.Random;
+
+public class PotionOfRecallLootFunction extends LootItemConditionalFunction {
+    protected PotionOfRecallLootFunction(LootItemCondition[] conditions) {
+        super(conditions);
+    }
+
+    @Override
+    protected ItemStack run(ItemStack stack, LootContext context) {
+        if (!Strange.LOADER.isEnabled(PotionOfRecall.class)) return stack;
+        ServerLevel level = context.getLevel();
+        Random random = context.getRandom();
+
+        if (random.nextFloat() > 0.5F) {
+            return stack;
+        }
+
+        // if the config value onlyOutsideOverworld is set, test the current dimension is not the overworld
+        if (PotionOfRecall.onlyOutsideOverworld && DimensionHelper.isOverworld(level)) {
+            return stack;
+        }
+
+        return PotionHelper.getPotionBottle(1, PotionOfRecall.RECALL_POTION);
+    }
+
+    @Override
+    public LootItemFunctionType getType() {
+        return PotionOfRecall.LOOT_FUNCTION;
+    }
+
+    public static class Serializer extends LootItemConditionalFunction.Serializer<PotionOfRecallLootFunction> {
+        @Override
+        public PotionOfRecallLootFunction deserialize(JsonObject json, JsonDeserializationContext context, LootItemCondition[] conditions) {
+            return new PotionOfRecallLootFunction(conditions);
+        }
+    }
+}
