003e91ee8cad1e865a7d1f4b1fbcde10e296d5b9
Sven Hjól <sven.hjol@protonmail.com>
2021-12-21 04:04:19 +0000

    Quest definition load/save nbt

--- src/main/java/svenhjol/strange/helper/NbtHelper.java
+++ src/main/java/svenhjol/strange/helper/NbtHelper.java
@@ -5,7 +5,9 @@ import net.minecraft.nbt.ListTag;
 import net.minecraft.nbt.StringTag;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class NbtHelper {
@@ -31,6 +33,109 @@ public class NbtHelper {
 
         for (int i = 0; i < list.size(); i++) {
             out.add(list.getString(i));
+        }
+
+        return out;
+    }
+
+    public static CompoundTag packMap(Map<String, String> map) {
+        var tag = new CompoundTag();
+
+        for (Map.Entry<String, String> entry : map.entrySet()) {
+            var key = entry.getKey();
+            var val = entry.getValue();
+            tag.putString(key, val);
+        }
+
+        return tag;
+    }
+
+    public static Map<String, String> unpackMap(CompoundTag tag) {
+        Map<String, String> out = new HashMap<>();
+
+        for (String key : tag.getAllKeys()) {
+            var val = tag.getString(key);
+            out.put(key, val);
+        }
+
+        return out;
+    }
+
+    public static CompoundTag packDoubleNested(Map<String, Map<String, String>> map) {
+        var tag = new CompoundTag();
+
+        for (Map.Entry<String, Map<String, String>> entry : map.entrySet()) {
+            var key1 = entry.getKey();
+            var tag1 = new CompoundTag();
+
+            for (Map.Entry<String, String> val1 : entry.getValue().entrySet()) {
+                var key2 = val1.getKey();
+                var val2 = val1.getValue();
+
+                tag1.putString(key2, val2);
+            }
+            tag.put(key1, tag1);
+        }
+
+        return tag;
+    }
+
+    public static Map<String, Map<String, String>> unpackDoubleNested(CompoundTag tag) {
+        Map<String, Map<String, String>> out = new HashMap<>();
+
+        for (String key1 : tag.getAllKeys()) {
+            var tag1 = tag.getCompound(key1);
+
+            for (String key2 : tag1.getAllKeys()) {
+                var val2 = tag1.getString(key2);
+
+                out.computeIfAbsent(key1, m -> new HashMap<>()).put(key2, val2);
+            }
+        }
+
+        return out;
+    }
+
+    public static CompoundTag packTripleNested(Map<String, Map<String, Map<String, String>>> map) {
+        var tag = new CompoundTag();
+
+        for (Map.Entry<String, Map<String, Map<String, String>>> entry : map.entrySet()) {
+            var key1 = entry.getKey();
+            var tag1 = new CompoundTag();
+
+            for (Map.Entry<String, Map<String, String>> val1 : entry.getValue().entrySet()) {
+                var key2 = val1.getKey();
+                var tag2 = new CompoundTag();
+
+                for (Map.Entry<String, String> val2 : val1.getValue().entrySet()) {
+                    var key3 = val2.getKey();
+                    var val3 = val2.getValue();
+
+                    tag2.putString(key3, val3);
+                }
+                tag1.put(key2, tag2);
+            }
+            tag.put(key1, tag1);
+        }
+
+        return tag;
+    }
+
+    public static Map<String, Map<String, Map<String, String>>> unpackTripleNested(CompoundTag tag) {
+        Map<String, Map<String, Map<String, String>>> out = new HashMap<>();
+
+        for (String key1 : tag.getAllKeys()) {
+            var tag1 = tag.getCompound(key1);
+
+            for (String key2 : tag1.getAllKeys()) {
+                var tag2 = tag1.getCompound(key2);
+
+                for (String key3 : tag2.getAllKeys()) {
+                    var val3 = tag2.getString(key3);
+
+                    out.computeIfAbsent(key1, m -> new HashMap<>()).computeIfAbsent(key2, m -> new HashMap<>()).put(key3, val3);
+                }
+            }
         }
 
         return out;
--- src/main/java/svenhjol/strange/module/quests/definition/QuestDefinition.java
+++ src/main/java/svenhjol/strange/module/quests/definition/QuestDefinition.java
@@ -1,7 +1,9 @@
 package svenhjol.strange.module.quests.definition;
 
 import com.google.gson.Gson;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.server.packs.resources.Resource;
+import svenhjol.strange.helper.NbtHelper;
 import svenhjol.strange.module.runes.Tier;
 
 import java.io.InputStreamReader;
@@ -12,23 +14,69 @@ import java.util.List;
 import java.util.Map;
 
 public class QuestDefinition {
+    public static final String TIER_TAG = "Tier";
+    public static final String ID_TAG = "Id";
+    public static final String TEST_TAG = "Test";
+    public static final String MODULES_TAG = "Modules";
+    public static final String DIMENSIONS_TAG = "Dimension";
+    public static final String LANG_TAG = "Lang";
+    public static final String HUNT_TAG = "Hunt";
+    public static final String GATHER_TAG = "Gather";
+    public static final String EXPLORE_TAG = "Explore";
+    public static final String BOSS_TAG = "Explore";
+    public static final String REWARD_TAG = "Reward";
+
+    private boolean test;
     private int tier;
     private String id;
-    private boolean test;
     private List<String> modules = new ArrayList<>();
     private List<String> dimensions = new ArrayList<>();
     private Map<String, Map<String, String>> lang = new HashMap<>();
-
-    // TODO: why are you this
-    private final Map<String, String> hunt = new HashMap<>();
-    private final Map<String, Map<String, Map<String, String>>> gather = new HashMap<>();
-    private final Map<String, Map<String, Map<String, String>>> explore = new HashMap<>();
-    private final Map<String, Map<String, Map<String, String>>> boss = new HashMap<>();
-    private final Map<String, Map<String, Map<String, String>>> reward = new HashMap<>();
+    private Map<String, String> hunt = new HashMap<>(); // TODO: why are you this
+    private Map<String, Map<String, Map<String, String>>> gather = new HashMap<>();
+    private Map<String, Map<String, Map<String, String>>> explore = new HashMap<>();
+    private Map<String, Map<String, Map<String, String>>> boss = new HashMap<>();
+    private Map<String, Map<String, Map<String, String>>> reward = new HashMap<>();
 
     public static QuestDefinition deserialize(Resource resource) {
         Reader reader = new InputStreamReader(resource.getInputStream());
         return new Gson().fromJson(reader, QuestDefinition.class);
+    }
+
+    public CompoundTag save() {
+        var tag = new CompoundTag();
+
+        tag.putInt(TIER_TAG, tier);
+        tag.putString(ID_TAG, id);
+        tag.putBoolean(TEST_TAG, test);
+        tag.put(MODULES_TAG, NbtHelper.packStrings(modules));
+        tag.put(DIMENSIONS_TAG, NbtHelper.packStrings(dimensions));
+        tag.put(HUNT_TAG, NbtHelper.packMap(hunt));
+        tag.put(LANG_TAG, NbtHelper.packDoubleNested(lang));
+        tag.put(GATHER_TAG, NbtHelper.packTripleNested(gather));
+        tag.put(EXPLORE_TAG, NbtHelper.packTripleNested(explore));
+        tag.put(BOSS_TAG, NbtHelper.packTripleNested(boss));
+        tag.put(REWARD_TAG, NbtHelper.packTripleNested(reward));
+
+        return tag;
+    }
+
+    public static QuestDefinition load(CompoundTag tag) {
+        var definition = new QuestDefinition();
+
+        definition.tier = tag.getInt(TIER_TAG);
+        definition.id = tag.getString(ID_TAG);
+        definition.test = tag.getBoolean(TEST_TAG);
+        definition.modules = NbtHelper.unpackStrings(tag.getCompound(MODULES_TAG));
+        definition.dimensions = NbtHelper.unpackStrings(tag.getCompound(DIMENSIONS_TAG));
+        definition.hunt = NbtHelper.unpackMap(tag.getCompound(HUNT_TAG));
+        definition.lang = NbtHelper.unpackDoubleNested(tag.getCompound(LANG_TAG));
+        definition.gather = NbtHelper.unpackTripleNested(tag.getCompound(GATHER_TAG));
+        definition.explore = NbtHelper.unpackTripleNested(tag.getCompound(EXPLORE_TAG));
+        definition.boss = NbtHelper.unpackTripleNested(tag.getCompound(BOSS_TAG));
+        definition.reward = NbtHelper.unpackTripleNested(tag.getCompound(REWARD_TAG));
+
+        return definition;
     }
 
     public String getId() {
@@ -81,5 +129,13 @@ public class QuestDefinition {
 
     public void setTier(Tier tier) {
         this.tier = tier.getLevel();
+    }
+
+    private CompoundTag packList(List<String> list) {
+        return NbtHelper.packStrings(list);
+    }
+
+    private List<String> unpackList(CompoundTag tag) {
+        return NbtHelper.unpackStrings(tag);
     }
 }
