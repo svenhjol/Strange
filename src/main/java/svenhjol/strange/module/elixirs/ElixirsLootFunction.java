e2fd418ff918544703582a86deb450606bf06e4e
Sven Hjól <sven.hjol@protonmail.com>
2021-12-26 20:45:13 +0000

    Elixirs generate 50% less frequently

--- src/main/java/svenhjol/strange/module/elixirs/ElixirsLootFunction.java
+++ src/main/java/svenhjol/strange/module/elixirs/ElixirsLootFunction.java
@@ -10,9 +10,6 @@ import net.minecraft.world.level.storage.loot.predicates.LootItemCondition;
 import svenhjol.strange.Strange;
 import svenhjol.strange.module.elixirs.elixir.Elixir;
 
-import java.util.Optional;
-import java.util.Random;
-
 public class ElixirsLootFunction extends LootItemConditionalFunction {
     protected ElixirsLootFunction(LootItemCondition[] conditions) {
         super(conditions);
@@ -26,17 +23,22 @@ public class ElixirsLootFunction extends LootItemConditionalFunction {
     }
 
     private ItemStack tryCreate(ItemStack stack, LootContext context) {
-        Random random = context.getRandom();
-
         if (Elixirs.POTIONS.isEmpty()) return stack;
 
+        var random = context.getRandom();
+
+        // 50% chance of not generating at all.
+        if (random.nextFloat() > 0.5F) {
+            return stack;
+        }
+
         // prefer the generic potions
-        Optional<IElixir> genericPotion = Elixirs.POTIONS.stream().filter(p -> p.getClass() == Elixir.class).findFirst();
+        var genericPotion = Elixirs.POTIONS.stream().filter(p -> p.getClass() == Elixir.class).findFirst();
         if (genericPotion.isPresent() && random.nextFloat() < 0.75F) {
             return genericPotion.get().getPotionItem();
         }
 
-        IElixir potion = Elixirs.POTIONS.get(random.nextInt(Elixirs.POTIONS.size()));
+        var potion = Elixirs.POTIONS.get(random.nextInt(Elixirs.POTIONS.size()));
         return potion.getPotionItem();
     }
 
