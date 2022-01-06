a06016769b7eaf49104035379e0334990ee7c9af
Sven Hjól <sven.hjol@protonmail.com>
2022-01-06 20:42:22 +0000

    Respect nether coords

--- src/main/java/svenhjol/strange/module/teleport/runic/handler/DimensionTeleportHandler.java
+++ src/main/java/svenhjol/strange/module/teleport/runic/handler/DimensionTeleportHandler.java
@@ -2,6 +2,7 @@ package svenhjol.strange.module.teleport.runic.handler;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.Level;
 import svenhjol.strange.module.runes.RuneBranch;
 
 public class DimensionTeleportHandler extends BaseTeleportHandler<ResourceLocation> {
@@ -11,8 +12,25 @@ public class DimensionTeleportHandler extends BaseTeleportHandler<ResourceLocati
 
     @Override
     public boolean process() {
-        BlockPos target = entity.blockPosition();
-        ResourceLocation dimension = value;
-        return teleport(dimension, target, false, true);
+        BlockPos targetPos;
+        var targetDimension = value;
+        var p = entity.blockPosition();
+
+        // Handle Nether size compression/expansion.
+        var nether = Level.NETHER.location();
+        if (!originDimension.equals(nether) && targetDimension.equals(nether)) {
+
+            targetPos = new BlockPos(p.getX() / 8, p.getY(), p.getZ() / 8);
+
+        } else if (originDimension.equals(nether) && !targetDimension.equals(nether)) {
+
+            targetPos = new BlockPos(p.getX() * 8, p.getY(), p.getZ() * 8);
+
+        } else {
+
+            targetPos = p;
+        }
+
+        return teleport(targetDimension, targetPos, false, true);
     }
 }
