1de99d5f6e7c0e447afea44530f802bff3644510
Sven H <sven.hjol@protonmail.com>
2023-09-26 00:11:27 +0000

    Register nether discs

--- src/main/java/svenhjol/strange/feature/ambient_music_discs/AmbientMusicDiscs.java
+++ src/main/java/svenhjol/strange/feature/ambient_music_discs/AmbientMusicDiscs.java
@@ -16,6 +16,7 @@ public class AmbientMusicDiscs extends CharmFeature {
     public static Map<String, Supplier<AmbientRecordItem>> items = new HashMap<>();
     public static Map<String, Supplier<SoundEvent>> sounds = new HashMap<>();
     public static final List<String> TRACKS = List.of(
+        "chrysopoeia",
         "creative1",
         "creative2",
         "creative3",
@@ -25,7 +26,13 @@ public class AmbientMusicDiscs extends CharmFeature {
         "menu1",
         "menu2",
         "menu3",
-        "menu4"
+        "menu4",
+        "nether1",
+        "nether2",
+        "nether3",
+        "nether4",
+        "rubedo",
+        "so_below"
     );
 
     @Configurable(name = "Attenuation support", description = "If true, ambient music discs will get quieter as the player moves away from the jukebox. EXPERIMENTAL!")
