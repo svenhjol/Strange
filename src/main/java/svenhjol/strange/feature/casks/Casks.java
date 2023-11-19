dc7b0f40ecb09c81bbdbc20bc4dc59e9b8cf9a44
Sven H <sven.hjol@protonmail.com>
2023-11-19 23:35:20 +0000

    Added missing description for casks

--- src/main/java/svenhjol/strange/feature/casks/Casks.java
+++ src/main/java/svenhjol/strange/feature/casks/Casks.java
@@ -29,6 +29,14 @@ public class Casks extends CommonFeature {
     public static boolean allowSplashAndLingering = false;
 
     @Override
+    public String description() {
+        return """
+               Casks are used to hold potions. Use a potion bottle on a cask to add the bottle's contents to the cask.
+               Use an empty bottle to retrieve one potion bottle back from the cask.
+               Potion effects are combined inside the cask and the contents are not lost when the cask is broken.""";
+    }
+
+    @Override
     public void register() {
         var registry = mod().registry();
 
