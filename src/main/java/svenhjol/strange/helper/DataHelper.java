6029e350b8d2f7b8962d104d9bb5a342901ed3c8
Sven H <sven.hjol@protonmail.com>
2024-01-30 23:43:05 +0000

    Fix wrong logic

--- src/main/java/svenhjol/strange/helper/DataHelper.java
+++ src/main/java/svenhjol/strange/helper/DataHelper.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public class DataHelper {
     public static boolean hasRequiredFeatures(List<ResourceLocation> features) {
-        return !features.isEmpty() && !features.stream().allMatch(
+        return features.isEmpty() || features.stream().allMatch(
             f -> Mods.optionalCommon(f.getNamespace())
                 .map(m -> m.loader().isEnabled(f.getPath()))
                 .orElse(false));
