0c4bf07b8f83f22dfadd7b33ac609b76e019dbc0
Sven H <sven.hjol@protonmail.com>
2024-02-18 20:49:05 +0000

    Only ever try and enchant "enchanted_book"

--- src/main/java/svenhjol/strange/feature/quests/reward/function/EnchantBook.java
+++ src/main/java/svenhjol/strange/feature/quests/reward/function/EnchantBook.java
@@ -1,5 +1,6 @@
 package svenhjol.strange.feature.quests.reward.function;
 
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import svenhjol.strange.feature.quests.reward.RewardItem;
@@ -28,22 +29,20 @@ public class EnchantBook implements RewardItemFunction {
         var stack = reward.stack;
         var random = quest.random();
 
-        var enchantLevel = quest.villagerLevel() * 6;
-
-        if (random.nextDouble() < params.chance
-            && !stack.isEnchanted()
-            && (stack.is(Items.BOOK) || stack.is(Items.ENCHANTED_BOOK))) {
-            EnchantmentHelper.enchantItem(random, stack, enchantLevel, params.allowTreasure);
+        if (stack.is(Items.ENCHANTED_BOOK)) {
+            reward.stack = EnchantmentHelper.enchantItem(random, new ItemStack(Items.BOOK, stack.getCount()),
+                params.enchantLevel, params.allowTreasure);
         }
     }
 
     public static class EnchantBookParameters {
-        public final double chance;
         public final boolean allowTreasure;
+        public final int enchantLevel;
 
         public EnchantBookParameters(RewardItemFunctionParameters params) {
-            this.chance = params.getDouble("chance", 0.5d);
             this.allowTreasure = params.getBoolean("allow_treasure", false);
+            this.enchantLevel = params.getInteger("level",
+                params.functionDefinition().questDefinition().level() * 6);
         }
     }
 }
